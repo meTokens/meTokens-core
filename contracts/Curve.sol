11a796b627e6a7c52b56814cd7b81c8956cb0abd
Carl Farterson <carlfarterson@gmail.com>
2021-02-02 20:25:39 -0600

    Create curve library

---
+++ contracts/Curve.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+library Curve {
+
+    function calculateMintReturn(
+        uint256 supply,
+        uint256 connectorBalance,
+        uint32 connectorWeight,
+        uint256 amount
+    ) returns (uint256) {
+
+    }
+
+    function calculateBurnReturn(
+        uint256 supply,
+        uint256 connectorBalance,
+        uint32 connectorWeight,
+        uint256 amount
+    ) returns (uint256) {
+
+    }
+}
