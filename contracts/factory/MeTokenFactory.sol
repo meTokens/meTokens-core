1d00c155f3c8ebf8e439a9ae9f47f42e4f5d918f
Carl Farterson <carlfarterson@gmail.com>
2021-02-06 23:18:27 -0800

    Update: Factory spacing

--- contracts/factory/MeTokenFactory.sol
+++ contracts/factory/MeTokenFactory.sol
@@ -4,12 +4,7 @@ import "../MeToken.sol";
 
 contract MeTokenFactory {
     
-    event MeTokenCreated(
-        address indexed _meToken,
-        address indexed _owner,
-        string _name,
-        string _symbol
-    );
+    event MeTokenCreated(address indexed _meToken, address indexed _owner, string _name, string _symbol);
 
     struct MeTokenDetails {
         address _tokenAddress,
@@ -19,9 +14,12 @@ contract MeTokenFactory {
     }
 
     address public owner;
-    MeTokenDetails[] public meTokens;
+    mapping(address => bool) private owners;
 
+    MeTokenDetails[] public meTokens;
+    
     Metoken public meToken;
+
 
     constructor (address _owner) {
         owner = owner;
@@ -33,24 +31,20 @@ contract MeTokenFactory {
         string _symbol
     ) public view returns (address _meToken) {
 
+        require(!isOwner(_owner), "initialize: address has already created their meToken");
+
         meToken m = new meToken(_owner, _name, _symbol);
-        meTokenDetail = MeTokenDetails(
-            m,
-            _owner,
-            _name,
-            _symbol
-        );
+        meTokenDetail = MeTokenDetails(m, _owner, _name, _symbol);
 
         meTokens.push(meTokenDetails);
         
-        emit meTokenCreated(
-            m,
-            _owner,
-            _name,
-            _symbol
-        );
+        emit meTokenCreated(m, _owner, _name, _symbol);
 
         return m;
+    }
+
+    function isOwner(address _address) public view returns (bool) {
+        return owners[_address];
     }
 
 
