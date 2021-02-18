698097c5114e7503e920508c59cd4aacdc1c83fa
Christopher Robison <cbobrobison@gmail.com>
2021-02-18 15:59:59 -0800

    Delete fees.sol

--- contracts/fees.sol
+++
@@ -1,9 +0,0 @@
-contract Fees {
-	
-	uint initilizeFee; // when a new meToken ERC20 is initilized (deployed) from factory.sol
-	uint mintFee; // when new meTokens are minted (supply increased)
-	uint burnFee; // when existing meTokens are burned (supply decreased) by non-owners
-	uint spendFee; // when meTokens are sent (spent) by non-owners to owners
-	uint earnFee; // when existing meTokens are burned by owners
-	uint yieldFee; // when vault funds yield APY
-}
