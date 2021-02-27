01a08699f5e6468abc8408d3252c265ce835766d
Carl Farterson <carlfarterson@gmail.com>
2021-02-27 10:10:27 -0600

    Add: shell Vault_singleAsset

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,2 +1,23 @@
 pragma solidity ^0.8.0;
 
+contract Vault_SingleAsset {
+
+	uint256 hub;
+	address collateralAsset;
+
+	mapping (address => MeTokenBalances) MeTokenBalances;
+
+	struct MeTokenBalances{
+		uint256 balancePooled;
+		uint256 balanceLocked;
+	}
+
+	/**
+	 * TODO - figure out governance of updating the collateralAsset in a vault
+	**/
+	function updateCollateralAsset () onlyGov returns() {}
+
+	/**
+	 * passes _valueSet through hub.curveOption.values.calculateMintReturn() and ~.calculateBurnReturn()
+	**/
+	function mint(_valueSet, _meToken) returns() {}
