83856838e94e484124bf50efa678eedb50b975ef
Carl Farterson <carlfarterson@gmail.com>
2021-03-03 17:46:26 -0500

    Add: calculateFee() and calculateLockedReturn()

--- contracts/VaultOptions/EXAMPLE - Vault_SingleAsset.sol
+++ contracts/VaultOptions/EXAMPLE - Vault_SingleAsset.sol
@@ -20,4 +20,14 @@ contract Vault_SingleAsset {
 	**/
 	function mint(_valueSet, _meToken) returns(){}
 	function burn(_valueSet, _meToken) returns(){}
+
+    /// @notice calculateFee is used to calculate the fee earned by the StakeOnMe Development Team whenever a MeToken Purchase or sale occurs throught contract
+    function calculateFee(uint256 amountEth) returns (uint256) {
+        return amountEth * percent / PRECISION;
+    }
+
+    /// @notice calculateLockedReturn is used to calculate the amount of locked Eth returned to the owner during a burn/spend
+    function calculateLockedReturn(uint256 amountToken, uint256 lockedBalance, uint256 supply) returns (uint256) {
+        return amountToken * lockedBalance / supply;
+    }
 }
