9b479333ac5a23c47a5e075de85aa84bfbe3e66e
Carl Farterson <carlfarterson@gmail.com>
2021-03-05 14:55:48 -0600

    Fix: do a bunch of shit

--- contracts/Fees.sol
+++ contracts/Fees.sol
@@ -2,126 +2,102 @@ pragma solidity ^0.8.0;
 
 contract Fees {
 
+    // NOTE: this will be the DAO
 	modifier onlyOwner() {
 		require(msg.sender == owner);
 		_;
 	}
 
-	event NewFee(string feeName, uint256 feeId, uint256 rate);
-	event DeactivateFee(string feeName, uint256 feeId);
-	event ReactivateFee(string feeName, uint256 feeId);
 	event SetMintFee(uint256 rate);
-	event SetBurnFee(uint256 rate);
-	event SetEarnFee(uint256 rate);
-	event SetOtherFee(string feeName, uint256 feeId, uint256 rate);
-	event NewFeeRecipient(string feeRecipient);
+	event SetBurnBuyerFee(uint256 rate);
+	event SetBurnOwnerFee(uint256 rate);
+    event SetTransferFee(uint256 rate);
+    event SetInterestFee(uint256 rate);
+    event SetYieldFee(uint256 rate);
+    event SetOwner(address owner);
+	event SetFeeRecipient(address recipient);
 
+    uint256 private FEE_MAX = 10**18;
 	/// @dev for when a meToken is minted
-	uint256 private _mintFee;
+	uint256 public mintFee;
 	/// @dev for when a meToken is burned by non-owner
-	uint256 private _burnFee;
+	uint256 public burnBuyerFee;
 	/// @dev for when a meToken is burned by owner
-	uint256 private _earnFee;
-
-	uint256 private MINTFEE_MIN = 0;
-	uint256 private MINTFEE_MAX = 10;
-	uint256 private BURNFEE_MIN = 0;
-	uint256 private BURNFEE_MAX = 10;
-	uint256 private EARNFEE_MIN = 0;
-	uint256 private EARNFEE_MAX = 10;
+	uint256 public burnOwnerFee;
+    /// @dev for when a meToken is transferred
+	uint256 public transferFee;
+    /// @dev Generated from interest on collateral
+    uint256 public interestFee;
+    /// @dev Generated from liquidity mining
+	uint256 public yieldFee;
 
     address public owner;
+	address public feeRecipient;
 
-	uint256 feeCount;
-	mapping (uint256 => Fee) fees;
-	
-	struct Fee {
-		string feeName;
-		uint256 rate;
-		uint256 min;
-		uint256 max;
-		bool active;
-	}
+	constructor(
+        address _owner,
+        uint256 _mintFee
+        uint256 _burnBuyerFee
+        uint256 _burnOwnerFee
+        uint256 _transferFee
+        uint256 _interestFee
+        uint256 _yieldFee
+    ) public {
+        owner = _owner;
+        mintFee = _mintFee;
+        burnBuyerFee = _burnBuyerFee;
+        burnOwnerFee = _burnOwnerFee;
+        transferFee = _transferFee;
+        interestFee = _interestFee;
+        yieldFee = _yieldFee;
+    }
 
-	address feeRecipient; 
-
-	constructor(){}
-
-	/// @dev to create a new type of fee for future vaults
-	function registerFee(string _feeName, uint256 _rate, uint256 _min, uint256 _max) onlyOwner {
-		Fee memory f = fees[feeCount];
-		f.feeName = _feeName;
-		f.rate = _rate;
-		f.min = _min;
-		f.max = _max;
-		f.active = true;
-		emit NewFee (_feeName, feeCount _rate);
-		feeCount++;
-	}
-
-	function deactivateFee(uint256 _feeId) onlyOwner {
-		Fee memory f = fees[_feeId];
-		f.active = false;
-		emit DeactivateFee(_feeName _feeId)
-	}
-
-	function reactivateFee(uint256 _feeId) OnlyOwner {
-		Fee memory f = fees[_feeId];
-		f.active = true;
-		emit ReactivateFee(_feeName _feeId)
-	}
-
-	function setMintFee(uint256 amount) external onlyOwner returns (uint256) {
-		require(amount >= MINTFEE_MIN && amount <= MINTFEE_MAX, "out of range");
-		_mintFee = amount;
+	function setMintFee(uint256 amount) external onlyOwner {
+        require(amount != mintFee && amount < FEE_MAX, "out of range");
+		mintFee = amount;
 		emit SetMintFee(amount);
-		return amount;
 	}
 
-	function setBurnFee(uint256 amount) external onlyOwner returns (uint256) {
-		require(amount >= BURNFEE_MIN && amount <= BURNFEE_MAX, "out of range");
-		_burnFee = amount;
-		emit SetBurnFee(amount);
-		return amount;
+	function setBurnBuyerFee(uint256 amount) external onlyOwner {
+        require(amount != burnBuyerFee && amount < FEE_MAX, "out of range");
+		burnBuyerFee = amount;
+		emit SetBurnBuyerFee(amount);
 	}
 
-	function setEarnFee(uint256 amount) external onlyOwner returns (uint256) {
-		require(amount >= EARNFEE_MIN && amount <= EARNFEE_MAX, "out of range");
-		_transferFee = amount;
+	function setBurnOwnerFee(uint256 amount) external onlyOwner {
+        require(amount != burnOwnerFee && amount < FEE_MAX, "out of range");
+		burnOwnerFee = amount;
+		emit SetBurnOwnerFee(amount);
+	}
+
+    function setTransferFee(uint256 amount) external onlyOwner {
+        require(amount != burnOwnerFee && amount < FEE_MAX, "out of range");
+		transferFee = amount;
 		emit SetTransferFee(amount);
-		return amount;
 	}
 
-	// TODO: complete logic
-	function setOtherFee(uint256 amount) {
-
-		emit setOtherFee(amount);
+    function setInterestFee(uint256 amount) external onlyOwner {
+        require(amount != interestFee && amount < FEE_MAX, "out of range");
+		interestFee = amount;
+		emit SetInterestFee(amount);
 	}
 
-	function setFeeRecipient(address _recipient) onlyOnwer {
+    function setYieldFee(uint256 amount) external onlyOwner {
+        require(amount != yieldFee && amount < FEE_MAX, "out of range");
+		yieldFee = amount;
+		emit SetYieldFee(amount);
+	}
+
+    function setOwner(address _owner) external onlyOwner {
+        require(_owner != owner, "_owner == owner");
+        owner = _owner;
+        emit SetOwner(_owner);
+    }
+
+	function setFeeRecipient(address _recipient) onlyOwner {
+        require(feeRecipient != _recipient, "feeRecipient == _recipient");
 		feeRecipient = _recipient;
-		emit NewFeeRecipient(_recipient);
+		emit SetFeeRecipient(_recipient);
 	}
 
-	/// @dev for when a meToken is minted
-	function mintFee() external view returns (uint256) {
-		return _mintFee;
-	}
-	/// @dev for when a meToken is burned by non-owner
-	function burnFee() external view returns (uint256) {
-		return _burnFee;
-	}
-	/// @dev for when a meToken is burned by owner
-	function earnFee() external view returns (uint256) {
-		return _earnFee;
-	}
-	function otherFee(uint256 _feeId) external view returns (uint256) {
-		Fee memory f = fees[_feeId];
-		require(f.active);
-		return f.rate;
-	}
-
-	function feeRecipient() external view returns (address) {
-		return feeRecipient;
-	}
 }
