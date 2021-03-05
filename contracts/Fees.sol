3ee94afeea1c8a9491d24a796490ba43e9333fd5
Carl Farterson <carlfarterson@gmail.com>
2021-03-05 15:03:24 -0600

    Fix: turn variables private

--- contracts/Fees.sol
+++ contracts/Fees.sol
@@ -19,72 +19,72 @@ contract Fees {
 
     uint256 private FEE_MAX = 10**18;
 	/// @dev for when a meToken is minted
-	uint256 public mintFee;
+	uint256 private _mintFee;
 	/// @dev for when a meToken is burned by non-owner
-	uint256 public burnBuyerFee;
+	uint256 private _burnBuyerFee;
 	/// @dev for when a meToken is burned by owner
-	uint256 public burnOwnerFee;
+	uint256 private _burnOwnerFee;
     /// @dev for when a meToken is transferred
-	uint256 public transferFee;
+	uint256 private _transferFee;
     /// @dev Generated from interest on collateral
-    uint256 public interestFee;
+    uint256 private _interestFee;
     /// @dev Generated from liquidity mining
-	uint256 public yieldFee;
+	uint256 private _yieldFee;
 
     address public owner;
 	address public feeRecipient;
 
 	constructor(
-        address _owner,
-        uint256 _mintFee
-        uint256 _burnBuyerFee
-        uint256 _burnOwnerFee
-        uint256 _transferFee
-        uint256 _interestFee
-        uint256 _yieldFee
+        address owner_,
+        uint256 mintFee_
+        uint256 burnBuyerFee_
+        uint256 burnOwnerFee_
+        uint256 transferFee_
+        uint256 interestFee_
+        uint256 yieldFee_
     ) public {
-        owner = _owner;
-        mintFee = _mintFee;
-        burnBuyerFee = _burnBuyerFee;
-        burnOwnerFee = _burnOwnerFee;
-        transferFee = _transferFee;
-        interestFee = _interestFee;
-        yieldFee = _yieldFee;
+        owner = owner_;
+        _mintFee = mintFee_;
+        _burnBuyerFee = burnBuyerFee_;
+        _burnOwnerFee = burnOwnerFee_;
+        _transferFee = transferFee_;
+        _interestFee = interestFee_;
+        _yieldFee = yieldFee_;
     }
 
 	function setMintFee(uint256 amount) external onlyOwner {
-        require(amount != mintFee && amount < FEE_MAX, "out of range");
-		mintFee = amount;
+        require(amount != _mintFee && amount < FEE_MAX, "out of range");
+		_mintFee = amount;
 		emit SetMintFee(amount);
 	}
 
 	function setBurnBuyerFee(uint256 amount) external onlyOwner {
-        require(amount != burnBuyerFee && amount < FEE_MAX, "out of range");
-		burnBuyerFee = amount;
+        require(amount != _burnBuyerFee && amount < FEE_MAX, "out of range");
+		_burnBuyerFee = amount;
 		emit SetBurnBuyerFee(amount);
 	}
 
 	function setBurnOwnerFee(uint256 amount) external onlyOwner {
-        require(amount != burnOwnerFee && amount < FEE_MAX, "out of range");
-		burnOwnerFee = amount;
+        require(amount != _burnOwnerFee && amount < FEE_MAX, "out of range");
+		_burnOwnerFee = amount;
 		emit SetBurnOwnerFee(amount);
 	}
 
     function setTransferFee(uint256 amount) external onlyOwner {
-        require(amount != burnOwnerFee && amount < FEE_MAX, "out of range");
-		transferFee = amount;
+        require(amount != _burnOwnerFee && amount < FEE_MAX, "out of range");
+		_transferFee = amount;
 		emit SetTransferFee(amount);
 	}
 
     function setInterestFee(uint256 amount) external onlyOwner {
-        require(amount != interestFee && amount < FEE_MAX, "out of range");
-		interestFee = amount;
+        require(amount != _interestFee && amount < FEE_MAX, "out of range");
+		_interestFee = amount;
 		emit SetInterestFee(amount);
 	}
 
     function setYieldFee(uint256 amount) external onlyOwner {
-        require(amount != yieldFee && amount < FEE_MAX, "out of range");
-		yieldFee = amount;
+        require(amount != _yieldFee && amount < FEE_MAX, "out of range");
+		_yieldFee = amount;
 		emit SetYieldFee(amount);
 	}
 
@@ -99,5 +99,30 @@ contract Fees {
 		feeRecipient = _recipient;
 		emit SetFeeRecipient(_recipient);
 	}
+
+    function mintFee() public view returns (uint256) {
+        return _mintFee;
+    }
+
+    function burnBuyerFee() public view returns (uint256) {
+        return _burnBuyerFee;
+    }
+
+    function burnOwnerFee() public view returns (uint256) {
+        return _burnOwnerFee;
+    }
+
+    function transferFee() public view returns (uint256) {
+        return _transferFee;
+    }
+
+    function interestFee() public view returns (uint256) {
+        return _interestFee;
+    }
+
+    function yieldFee() public view returns (uint256) {
+        return _yieldFee;
+    }
+
 
 }
