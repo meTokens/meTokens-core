b421a63443450318477e0e8bc7166800bdd81dc4
Carl Farterson <carlfarterson@gmail.com>
2021-03-05 15:54:19 -0600

    Create: Interfaces dir

---
+++ contracts/Interfaces/I_BancorZeroValues.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract I_CurveZeroValues {
+    function calculateMintReturn(
+        uint256 _hubId,
+        uint256 _supply,
+        uint256 _balancePooled,
+        uint256 _depositAmount
+    ) public view returns (uint256 amount);
+
+    function calculateBurnReturn(
+        uint256 _hubId,
+        uint256 _supply,
+        uint256 _balancePooled,
+        uint256 _sellAmount
+    ) public view returns (uint256 amount);
+}
