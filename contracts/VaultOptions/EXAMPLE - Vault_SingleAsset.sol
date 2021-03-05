4e4ea7f0584720e23e3cab4cf6ddb9e5aa18b54b
Christopher Robison <cbobrobison@gmail.com>
2021-03-05 12:57:18 -0800

    Updates

--- contracts/VaultOptions/EXAMPLE - Vault_SingleAsset.sol
+++ contracts/VaultOptions/EXAMPLE - Vault_SingleAsset.sol
@@ -1,4 +1,7 @@
-contract Vault_SingleAsset {
+import "../fees.sol";
+import "../HubRegistry/HubRegistry.sol";
+
+contract Vault_SingleAsset is Fees, MeToken, MeTokenRegistry, HubRegistry {
 
 	uint256 hub;
 	address collateralAsset;
@@ -6,8 +9,10 @@ contract Vault_SingleAsset {
 	mapping (address => MeTokenBalances) MeTokenBalances;
 
 	struct MeTokenBalances{
+		uint256 supply;
 		uint256 balancePooled;
 		uint256 balanceLocked;
+		bool subscribed;
 	}
 
 	/**
@@ -18,12 +23,35 @@ contract Vault_SingleAsset {
 	/**
 	 * passes _valueSet through hub.curveOption.values.calculateMintReturn() and ~.calculateBurnReturn()
 	**/
-	function mint(_valueSet, _meToken) returns(){}
-	function burn(_valueSet, _meToken) returns(){}
+	function mint(uint _amount, address _meToken) returns(){
+		uint256 fee = mintFee();
+	}
 
-    /// @notice calculateFee is used to calculate the fee earned by the StakeOnMe Development Team whenever a MeToken Purchase or sale occurs throught contract
-    function calculateFee(uint256 amountEth) returns (uint256) {
-        return amountEth * percent / PRECISION;
+	function burn(uint _amount, address _meToken) returns(){
+		MeTokenBalances memory mt = MeTokenBalances[_meToken];
+		uint burnReturn = calculateBurnReturn(/**hubValueSetId**/, mt.supply, mt.balancePooled, _amount); //figure out logic
+		address recipient = feeRecipient();
+		uint256 fee;
+		uint256 toUser;
+		uint256 toRecipient;
+		uint256 toLock;
+
+		if (msg.sender = meToken.owner){
+			fee = earnFee();
+			uint256 fromLocked = burnReturn / mt.supply * mt.lockedBalance;
+			toRecipient = fee * burnReturn;
+			toUser = burnReturn - toRecipient + fromLocked;
+		} else {
+			fee = burnFee();
+			toRecipient = fee * burnReturn;
+			toUser = burnReturn - toRecipient;
+			toLock = 
+		}
+		_meToken.transferFrom(msg.sender, toRecipient, toRecipient);
+		_meToken.transferFrom(msg.sender, toUser, toUser);
+		mt.balancePooled -- _amount;
+		mt.balanceLocked ++ 
+		mt.balanceLocked -- fromLocked;
     }
 
     /// @notice calculateLockedReturn is used to calculate the amount of locked Eth returned to the owner during a burn/spend
