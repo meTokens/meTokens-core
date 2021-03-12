73a12e5f93bd51bd78723552850649ec5fdd38a6
Carl Farterson <carlfarterson@gmail.com>
2021-03-12 14:53:22 -0800

    03.12.21 shit

--- contracts/Registries/HubRegistry.sol
+++ contracts/Registries/HubRegistry.sol
@@ -1,24 +1,40 @@
 pragma solidity ^0.8.0;
 
 import "../Interaces/I_VaultFactory.sol";
+import "../Interaces/I_VaultRegistry.sol";
 
 contract HubRegistry {
 
-	mapping (uint256 => HubDetails) hubs;
+    event RegisterHub(address factory, string name, uint256 hubId);
+    event DeactivateHub(uint256 hubId);
+    event ReactivateHub(uint256 hubId);
+
+	mapping (uint256 => HubDetails) private hubs;
     uint256 private _hubCount;
+    address public gov;
+    I_VaultRegistry public vaultRegistry;
 
     struct HubDetails {
     	string name;
         uint256 curveDetails;
         address vault;
+        address owner;
+        bool active;
+    }
+
+    constructor(address _vaultRegistry, address _gov) public {
+        require(_vaultRegistry != address(0), "_vaultRegistry cannot be 0 address");
+        vaultRegistry = I_VaultRegistry(_vaultRegistry);
+        gov = _gov;
     }
 
     function registerHub(
         string calldata _name,
         uint256 _curveDetails,
-        address _vaultFactory
+        address _vaultFactory,
     ) public {
-        // TODO: require _vaultFactory to be an approved factory
+        require(vaultRegistry.isApprovedVaultFactory(_vaultFactory), "_vaultFactory not approved");
+        
 
         // Create new vault
 
--- contracts/Registries/VaultRegistry.sol
+++ contracts/Registries/VaultRegistry.sol
@@ -5,33 +5,67 @@ contract VaultRegistry {
     event RegisterVault(address factory, string name, uint256 vaultId);
     event DeactivateVault(uint256 vaultId);
     event ReactivateVault(uint256 vaultId);
+    // event RegisterFactory(address factory);
+    // event DeactivateFactory(address factory);
 
-	mapping (uint256 => VaultDetails) vaults;
-    uint256 private _vaultCount;
+	mapping (uint256 => VaultDetails) vaultFactories;
+    mapping (address => bool) vaults;
+    uint256 private _vaultFactoryCount;
 
-    struct VaultDetails {
+    // struct VaultDetails {
+    //     address factory; // reference vaultBalancerFactory.sol as an example of a vault factory
+    // 	string name;
+    //     bool active;
+    // }
+
+    struct VaultFactoryDetails {
         address factory; // reference vaultBalancerFactory.sol as an example of a vault factory
-    	string name;
+        string name;
         bool active;
     }
 
     // TODO: argument check
     // TODO: access control
     function registerVault(address factory, string calldata name) {
+        require(vaultFactories[factory], "Factory is not registered factory");
+
+        // Create vault
+        
+
+        // Add vault details to storage
         VaultDetails memory v = VaultDetails(factory, name, true);
-        vaults[_vaultCount] = v;
-        emit RegisterVault(factory, name, _vaultCount);
-        _vaultCount++;
+        vaults[++_vaultFactoryCount] = v;
+
+        emit RegisterVault(factory, name, _vaultFactoryCount);
     }
-    
-    function deactivateVault(uint256 vaultId) {
+
+    function deactivateVault(uint256 vaultId) public {
         emit DeactivateVault(vaultId);
     }
-    function reactivateVault(uint256 vaultId) {
+    function reactivateVault(uint256 vaultId) public {
         emit ReactivateVault(vaultId);
     }
 
-    function vaultCount() public view returns (uint256) {
-        return _vaultCount;
+    // TODO: access control
+    function registerVaultFactory(address factory) public {
+        require(!vaultFactories[factory], "Factory already registered");
+        require(factory != address(0), "Factory cannot equal 0 address");
+        vaultFactories[factory] = true;
+        emit RegisterVaultFactory(factory);
     }
+
+    function deactivateVaultFactory(uint256 factory) public {
+        require(vaultFactories[factory], "Factory not registered");
+        vaultFactories[factory] = false;
+        emit DeactivateVaultFactory(vaultId);
+    }
+
+    function getVaultFactoryCount() public view returns (uint256) {
+        return _vaultFactoryCount;
+    }
+
+    function isApprovedVaultFactory(address factory) public view returns (bool) {
+        return vaultFactories[factory];
+    }
+
 }
