03af2429a8fc04323756c8c2f7c775689cbc4d24
Carl Farterson <carlfarterson@gmail.com>
2021-05-11 14:42:04 -0700

    Mv: withdrawFees() to single asset vault

--- contracts/vaults/SingleAsset.sol
+++ contracts/vaults/SingleAsset.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
+import "../interfaces/I_ERC20.sol";
 import "./Vault.sol";
+
 
 /// @title ERC-20 (non-LP) token vault
 /// @author Carl Farterson (@carlfarterson)
@@ -10,17 +12,33 @@ import "./Vault.sol";
 /// @dev Only callable by the vault factory
 contract SingleAsset is Vault, Initializable {
 
+    I_VaultRegistry public vaultRegistry = I_VaultRegistry(0x0); // TODO
+
     constructor() {}
 
     function initialize(
         address _owner,
         address _collateralAsset
-    ) initializer onlyVaultFactory public {
+    ) initializer public {
+        require(vaultRegistry.isApprovedVaultFactory(msg.sender), "msg.sender not approved vault factory");
+
         owner = _owner;
         collateralAsset = _collateralAsset;
 
         // Approve Foundry to spend all collateral in vault
         I_ERC20(collateralAsset).approve(foundry, 2**256 - 1);
+    }
+
+    function withdrawFees(bool _max, uint256 _amount, address _to) external {
+        require(msg.sender == gov, "!gov");
+        if (_max) {
+            _amount = accruedFees;
+        } else {
+            require(_amount <= accruedFees, "_amount cannot exceed feeBalance");
+        }
+
+        I_ERC20(collateralAsset).transfer(_to, _amount);
+        emit WithdrawFees(_amount, _to);
     }
 
 
--- contracts/vaults/Vault.sol
+++ contracts/vaults/Vault.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.0;
 
-import "../interfaces/I_ERC20.sol";
 import "../interfaces/I_VaultRegistry.sol";
 
 
@@ -9,17 +8,13 @@ import "../interfaces/I_VaultRegistry.sol";
 /// @notice Base vault contract inherited by all vaults
 contract Vault {
 
-    modifier onlyVaultFactory(address _from) {
-        require(vaultRegistry.isApprovedVaultFactory(msg.sender), "msg.sender not approved vault factory");
-        _;
-    }
-
     event WithdrawFees(uint256 amount, address to);
 
     uint256 private PRECISION = 10**18;
     address foundry = address(0x0);  // TODO
     address gov = address(0x0);  // TODO
     I_VaultRegistry public vaultRegistry = I_VaultRegistry(0x0); // TODO
+
 
     address public owner;
 	address private collateralAsset;
@@ -28,18 +23,5 @@ contract Vault {
     function getCollateralAsset() external view returns (address) {
         return collateralAsset;
     }
-
-    function withdrawFees(bool _max, uint256 _amount, address _to) external {
-        require(msg.sender == gov, "!gov");
-        if (_max) {
-            _amount = accruedFees;
-        } else {
-            require(_amount <= accruedFees, "_amount cannot exceed feeBalance");
-        }
-
-        I_ERC20(collateralAsset).transfer(_to, _amount);
-        emit WithdrawFees(_amount, _to);
-    }
-
 
 }
