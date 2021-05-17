d1bf9931e77afc2f5575c89c46f4bd70b9288694
Carl Farterson <carlfarterson@gmail.com>
2021-05-17 13:00:11 -0700

    Add: I_Migrations

---
+++ contracts/interfaces/I_Migrations.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+
+/// @title meToken Migrations
+/// @author Carl Farterson (@carlfarterson)
+/// @notice contract to manage migration settings
+interface I_Migrations {
+
+    function setMinBlocksUntilStart(uint256 blocks) external;
+    function setMaxBlocksUntilStart(uint256 blocks) external;
+
+    function setMinUpdateBlockDuration(uint256 blocks) external;
+    function setMaxUpdateBlockDuration(uint256 blocks) external;
+
+    function minBlocksUntilStart() external view returns (uint256);
+    function maxBlocksUntilStart() external view returns (uint256);
+    function minUpdateBlockDuration() external view returns (uint256);
+    function maxUpdateBlockDuration() external view returns (uint256);
+
+}
