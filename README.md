54ab279f01498d7f3e28e05057e8115e3540d91a
Carl Farterson <carlfarterson@gmail.com>
2021-05-18 17:19:11 -0700

    Wizardry

--- README.md
+++ README.md
@@ -17,6 +17,4 @@
 * [x] switch BancorZeroValueSet and Migrations from block number to timestamp
 * [x] Unencode `BancorZeroValueSet.updateValueSet`
 * [ ] Increment Vault.accruedFees from minting and burning
-    * For mint & burn, does curve calculate return based on amount or amountAfterFees? 
-    * For burn, is fee in meToken or collateralToken?
     * `fees.feeRecipient()` - should withdrawing fees only get sent to a specific address?
--- contracts/Foundry.sol
+++ contracts/Foundry.sol
@@ -46,13 +46,13 @@ contract Foundry {
             hub,
             meToken.totalSupply(),
             meTokenDetails.balancePooled,
-            collateralDepositedAfterFees // TODO: do we use this calculation or collateralDeposited
+            collateralDepositedAfterFees 
         );
         
-        // update balancePooled (TODO)
 
-        // Send collateral to vault
+        // Send collateral to vault and update balance pooled
         collateralToken.transferFrom(msg.sender, address(this), collateralDeposited);
+        meTokenDetails.balancePooled += collateralDepositedAfterFees; // TODO
 
         // Send fees to recipient
         if (fee > 0) {vault.addFee(fee);}
@@ -66,6 +66,10 @@ contract Foundry {
 
     function burn(address _meToken, uint256 _meTokensBurned) external {
 
+        uint256 feeRate;
+        uint256 burnMultiplier;
+        uint256 meTokensBurnedWeighted;
+
         uint256 hub = meTokenRegistry.getMeTokenHub(_meToken);
         HubDetails memory hubDetails = hubs[hub];
         require(hubDetails.status != "INACTIVE", "Hub inactive");
@@ -78,48 +82,41 @@ contract Foundry {
         I_CurveValueSet curve = I_CurveValueSet(huDetails.curve);
         I_Vault vault = I_Vault(hubDetails.vault);
         I_ERC20 collateralToken = I_ERC20(vault.getCollateralAsset());
-
-        uint256 fee;
-        uint256 burnReturn;
+        
+        // Calculate how many collateral tokens are returned
+        uint256 collateralReturned = curve.calculateBurnReturn(
+            hub,
+            meToken.totalSupply(),
+            meTokenDetails.balancePooled,
+            _meTokensBurned
+        );
 
         // If msg.sender == owner, give owner the sell rate.
         // If msg.sender != owner, give msg.sender the burn rate
         if (meTokenRegistry.getMeTokenOwner(_meToken) == msg.sender) {
-
-            fee = _meTokensBurned * fees.burnOwnerFee() / PRECISION;
-            uint256 earnedFromLocked = _meTokensBurned * meTokenDetails.balanceLocked / meToken.totalSupply();
-            burnReturn = _meTokensBurned - fee + earnedFromLocked;
-
-            // decrease balance locked (TODO)
-
+            feeRate = fees.burnOwnerFee();
+            collateralMultiplier = PRECISION + PRECISION * meTokenDetails.balanceLocked / meToken.totalSupply();
         } else {
-            fee = _meTokensBurned * fees.getBuyerFee() / PRECISION;
-            burnReturn = _meTokensBurned - fee;
-            uint256 meTokensToLock = burnReturn * hubDetails.refundRatio / PRECISION;
-
-            // increase balance locked (TODO)
-
+            feeRate = fees.burnBuyerFee();
+            collateralMultiplier = PRECISION - hubDetails.refundRatio;
         }
 
-        // Calculate how many collateral tokens are returned
-        uint256 collateralReturned = curve.calculateMintReturn(
-            hub,
-            meToken.totalSupply(),
-            meTokenDetails.balancePooled,
-            burnReturn
-        );
+        uint256 collateralReturnedWeighted = collateralReturned * collateralMultiplier / PRECISION;
+        uint256 fee = collateralReturnedWeighted * feeRate / PRECISION;
 
-        // TODO: Update balance pooled
+        uint256 collateralReturnedAfterFees = collateralReturnedWeighted - fee;
+
+        // TODO: Update balances pooled & locked
+        meTokenDetails.balancePooled -= collateralReturned;
+        meTokenDetails.balancedLocked += (collateralReturned - collateralReturnedWeighted);
 
         // Burn metoken from user
-        meToken.burn(msg.sender, burnReturn);
+        meToken.burn(msg.sender, _meTokensBurned);
 
         // Send collateral from vault
-        collateralAsset.transferFrom(hubDetails.vault, msg.sender, collateralReturned);        
+        collateralAsset.transferFrom(hubDetails.vault, msg.sender, collateralReturnedAfterFees);
 
         // Transfer fees
-        // TODO: Would fees be in meToken
-        meToken.transfer(fees.feeRecipient(), fee);
         if (fee > 0) {vault.addFee(fee);}
 
     }
