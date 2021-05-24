22285579c91a3f13a34decfd6bdee564aeca18b5
Carl Farterson <carlfarterson@gmail.com>
2021-05-24 07:13:11 -0700

    Add: onlyAuthorized()

--- contracts/MeToken.sol
+++ contracts/MeToken.sol
@@ -10,13 +10,15 @@ import "@openzeppelin/contracts/token/ERC20/ERCBurnable.sol";
 /// @notice Base erc20-like meToken contract used for all meTokens
 contract MeToken is Initializable, ERC20Burnable {
 
-    modifier onlyHub() {
-        require(msg.sender == hub, "!hub");
+    modifier onlyAuthorized() {
+        require(msg.sender == hub || msg.sender == meTokenRegistry , "!authorized");
+        _;
     }
 
     // For person that creates meToken, may not be the designated owner
     // For example, someone creates ANDRE coin on his behalf and he takes ownership later
-    address public hub;
+    address public hub = address(0x0); // TODO
+    address public meTokenRegistry = address(0x0); // TODO
 
     string public name;
     string public symbol;
@@ -28,20 +30,18 @@ contract MeToken is Initializable, ERC20Burnable {
     /// @param _name name of meToken
     /// @param _symbol symbol of meToken
     function initialize(
-        address _hub,
         string calldata _name,
         string calldata _symbol
     ) initializer public {
-        hub = _hub;
         name = _name;
         symbol = symbol;
     }
 
-    function mint(address to, uint256 amount) onlyHub external {
+    function mint(address to, uint256 amount) onlyAuthorized external {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 value) onlyHub external {
+    function burn(address from, uint256 value) onlyAuthorized external {
         _burn(from, value);
     }
 
