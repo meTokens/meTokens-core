c32292dd85ffebfdbe09be6a23279e9740cdfdad
Carl Farterson <carlfarterson@gmail.com>
2021-05-27 09:27:01 -0700

    Mv: contracts/{Delta => Updater}.sol

---
+++ contracts/Updater.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+/* NOTE: this contract is for...
+- reconfigure valuesets
+- shift refundratios
+- recollateralizing vaults
+- migrate curves
+- subscribe meTokens to new hubs
+*/
+
+contract Updater {
+
+    function reconfigure() external {
+
+    }
+
+    function shift() external {
+
+    }
+
+    function migrate() external {
+
+    }
+
+    function subscribe() external {
+        
+    }
+}
