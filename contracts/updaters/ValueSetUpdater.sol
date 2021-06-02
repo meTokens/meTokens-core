253c652ab6b33b46e6c299e4d33e3174e54aea35
Carl Farterson <carlfarterson@gmail.com>
2021-06-02 12:07:50 -0700

    Add: updater

--- contracts/updaters/ValueSetUpdater.sol
+++ contracts/updaters/ValueSetUpdater.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/I_Hub.sol";
-import "../interfaces/I_Migrations.sol"; // TODO
+import "../interfaces/I_Updater.sol"; // TODO
 
 contract ValueSetUpdater {
 
     I_Hub public hub;
-    I_Updater public migrations;
+    I_Updater public updater;
 
-    constructor(address _hub, address _migrations) {
+    constructor(address _hub, address _updater) {
         hub = _hub;
-        migrations = _migrations;
+        updater = _updater;
     }
 
 
@@ -21,19 +21,7 @@ contract ValueSetUpdater {
         uint256 _endTime
     ) external override {
 
-        // TODO: determine where to place these
-        require(
-            _startTime - block.timestamp >= migrations.minSecondsUntilStart() &&
-            _startTime - block.timestamp <= migrations.maxSecondsUntilStart(),
-            "Unacceptable _startTime"
-        );
-        require(
-            _endTime - _startTime >= migrations.minUpdateDuration() &&
-            _endTime - _startTime <= migrations.maxUpdateDuration(),
-            "Unacceptable update duration"
-        );
-
-        require(msg.sender == hub.getHubOwner(_hubId), "msg.sender not hub owner");
+        require(msg.sender == updater, "!updater");
         require(hub.getHubStatus(_hubId) == 2, "!ACTIVE");
 
         curve = I_Curve(hub.getHubCurve(_hubId));
