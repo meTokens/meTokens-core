80eb8f9727494680198c9afdfedc0f82731aaeca
Carl Farterson <carlfarterson@gmail.com>
2021-06-02 11:52:23 -0700

    Add: startUpdate()

--- contracts/updaters/Updater.sol
+++ contracts/updaters/Updater.sol
@@ -1,15 +1,65 @@
 pragma solidity ^0.8.0;
 
+import "./interfaces/I_Migrations.sol";
+import "./interfaces/I_Hub.sol";
+
+
 contract Updater {
 
     struct UpdateDetails {
-        bool reconfiguring;
+        // bool reconfiguring;
         bool migrating;
         bool shifting;
         bool recollateralizing;
         uint256 startTime;
         uint256 endTime;
     }
+
+    I_Migrations public migrations;
+    I_Hub public hub;
+
+    constructor(address _migrations, address _hub) {
+        I_Migrations migrations = I_Migrations(_migrations);
+        I_Hub hub = I_Hub(_hub);
+    }
+
+    // TODO: args
+    function startUpdate(
+        uint256 _hubId,
+        uint256 _startTime,
+        uint256 _endTime,
+        address _targetVault,
+        uint256 _targetRefundRatio,
+        address _targetCurve,
+        bytes32 _targetEncodedValueSet
+    ) external {
+
+        // TODO: access control
+
+        require(
+            _startTime - block.timestamp >= migrations.minSecondsUntilStart() &&
+            _startTime - block.timestamp <= migrations.maxSecondsUntilStart(),
+            "Unacceptable _startTime"
+        );
+        require(
+            _endTime - _startTime >= migrations.minUpdateDuration() &&
+            _endTime - _startTime <= migrations.maxUpdateDuration(),
+            "Unacceptable update duration"
+        );
+
+        // is valid curve
+
+        // is valid vault
+
+        // is valid refundRatio
+
+        // is valid targetValueSet
+
+        // TODO
+        hub.startUpdateStatus(_startTime, _endTime);
+
+    }
+
 
     function getUpdateDetails(uint256 _hubId) external view returns (address) {}
 
@@ -23,9 +73,14 @@ contract Updater {
         // TODO
     }
 
+    function getTargetVault(uint256 _hubId) external view returns (uint256) {
+        // TODO
+    }
 
 
-    // TODO: natspec
+
+
+    // TODO: figure out where the heck to put this
     function _calculateWeightedAmount(
         uint256 _amount,
         uint256 _targetAmount,
