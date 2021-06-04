e619007cdf314057bb3fca42ce2060be5ddf75e3
Carl Farterson <carlfarterson@gmail.com>
2021-06-03 18:27:31 -0700

    Del: redundant updater contracts

--- contracts/updaters/CurveUpdater.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract CurveUpdater {
-
-    function migrate() external {
-
-    }
-    
-}
--- contracts/updaters/VaultUpdater.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract VaultUpdater {
-
-    function recollateralize() external {
-
-    }
-    
-}
