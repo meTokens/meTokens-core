b517b6f0346ac0bb670cf56650b7adfa4f9565e6
Carl Farterson <carlfarterson@gmail.com>
2021-08-13 16:23:13 -0700

    Fix: struct -> enum

--- contracts/libs/Status.sol
+++ contracts/libs/Status.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-/*
 library Status {
 
-    struct Status {
+    enum Status {
         INACTIVE,
         ACTIVE,
         QUEUED,
         UPDATING
     }
-
 }
-*/
