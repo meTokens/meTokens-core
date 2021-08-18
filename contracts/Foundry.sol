9b47fb4ef53a63a3c8638eadf39272616b64a154
Carl Farterson <carlfarterson@gmail.com>
2021-08-18 03:45:57 -0700

    Del: commented code & fix variables

--- contracts/Foundry.sol
+++ contracts/Foundry.sol
@@ -28,59 +28,23 @@ contract Foundry is IFoundry, Ownable, Initializable {
     function initialize(
         address _hub,
         address _fees,
-        address _meTokenRegistry,
+        address _meTokenRegistry
     ) external onlyOwner initializer {
         hub = IHub(_hub);
         fees = IFees(_fees);
         meTokenRegistry = IMeTokenRegistry(_meTokenRegistry);
     }
 
-//    function mint(address _meToken, uint256 _collateralDeposited, address _recipient) external override {}
-//    function burn(address _meToken, uint256 _meTokensBurned , address _recipient) external override {}
-
-    // function mint(address _meToken, uint256 _collateralDeposited, address _recipient) external override {}
-
     /// @inheritdoc IFoundry
     function mint(address _meToken, uint256 _collateralDeposited, address _recipient) external override {
 
-//        uint256 id;
-//        uint256 balancePooled;
-//        uint256 balanceLocked;
-        // bool resubscribing;
         IMeTokenRegistry.Details memory details = meTokenRegistry.getDetails(_meToken);
 
-        // TODO: convert this handling logic to targetValueSet conditions
-        // require(!resubscribing, "meToken is resubscribing");
-
-//        uint256 hubStatus = hub.getStatus(id);
         require(hub.getStatus(details.id) != 0, "Hub inactive");
-
-//        bool reconfiguring;  // NOTE: this is done on the valueSet level
-//        address migrating;
-//        address recollateralizing;
-//        uint256 startTime;
-//        uint256 endTime;
-
-//        if (hubStatus > 1) { // QUEUED, UPDATING
-//            (reconfiguring, migrating, recollateralizing, , startTime, endTime) = updater.getDetails(id);
-//            if (hubStatus == 2 && block.timestamp > startTime) { // QUEUED
-//                // TODO: set hub status to UDPATING and trigger new vault if needed
-//                // updater.
-//            }
-//
-//            if (block.number > endTime) {
-//                // End update
-//                updater.finishUpdate(id);
-//                reconfiguring = false;
-//                migrating = address(0);
-//                recollateralizing = address(0);
-//            }
-//        }
 
         IERC20 meToken = IERC20(_meToken);
         ICurveValueSet curve = ICurveValueSet(hub.getCurve(details.id));
         IVault vault = IVault(hub.getVault(details.id));
-//        IERC20 collateralToken = IERC20(vault.getCollateralAsset());
 
         uint256 fee = _collateralDeposited * fees.mintFee() / PRECISION;
         uint256 collateralDepositedAfterFees = _collateralDeposited - fee;
@@ -97,39 +61,17 @@ contract Foundry is IFoundry, Ownable, Initializable {
             details.id,
             meToken.totalSupply(),
             details.balancePooled
-//            reconfiguring,
-//            startTime,
-//            endTime
         );
 
-//        if (migrating != address(0)) {
-//            // Do something
-//            ICurveValueSet targetCurve = ICurveValueSet(updater.getTargetCurve(id));
-//            uint256 targetMeTokensMinted = targetCurve.calculateMintReturn(
-//                collateralDepositedAfterFees,
-//                id,
-//                meToken.totalSupply(),
-//                balancePooled
-//            );
-//            meTokensMinted = WeightedAverage.calculate(
-//                meTokensMinted,
-//                targetMeTokensMinted,
-//                startTime,
-//                block.timestamp,
-//                endTime
-//            );
-//        }
-
-        // Send collateral to new vault and update balance pooled
-//        if (recollateralizing != address(0)) {
-//            collateralToken.transferFrom(msg.sender, recollateralizing, _collateralDeposited);
-//        }
+        // Send collateral to vault and update balance pooled
+        IERC20 collateralToken = IERC20(vault.getCollateralAsset());
+        collateralToken.transferFrom(msg.sender, address(this), _collateralDeposited);
 
         meTokenRegistry.incrementBalancePooled(
             true,
             _meToken,
             collateralDepositedAfterFees
-        ); // TODO: validate
+        );
 
         // Transfer fees
         if (fee > 0) {vault.addFee(fee);}
@@ -142,64 +84,20 @@ contract Foundry is IFoundry, Ownable, Initializable {
     /// @inheritdoc IFoundry
     function burn(address _meToken, uint256 _meTokensBurned , address _recipient) external override {
 
-//        address owner;
-//        uint256 id;
-//        uint256 balancePooled;
-//        uint256 balanceLocked;
-        // bool resubscribing;
-//        (owner, id, balancePooled, balanceLocked, ) = meTokenRegistry.getDetails(_meToken);
         IMeTokenRegistry.Details memory details = meTokenRegistry.getDetails(_meToken);
-        // TODO: convert this handling logic to targetValueSet conditions
-        // require(!resubscribing, "meToken is resubscribing");
 
-//        uint256 hubStatus = hub.getStatus(id);
         require(hub.getStatus(details.id) != 0, "Hub inactive");
 
-//        bool reconfiguring;
-//        address migrating;
-//        address recollateralizing;
-//        uint256 shifting;
-//        uint256 startTime;
-//        uint256 endTime;
-
-//        if (hubStatus == 2) {  // UPDATING
-//            if (block.timestamp > endTime) {
-//                updater.finishUpdate(id);
-//            } else {
-//                (reconfiguring, migrating, recollateralizing, shifting, startTime, endTime) = updater.getDetails(id);
-//            }
-//        }
-
-//        IERC20 meToken = IERC20(_meToken);
         ICurveValueSet curve = ICurveValueSet(hub.getCurve(details.id));
-//        IVault vault = IVault(hub.getVault(id));
-//        IERC20 collateralToken = IERC20(vault.getCollateralAsset());
+        IVault vault = IVault(hub.getVault(details.id));
 
         // Calculate how many collateral tokens are returned
         uint256 collateralReturned = curve.calculateBurnReturn(
             _meTokensBurned,
-                details.id,
+            details.id,
             IERC20(_meToken).totalSupply(),
             details.balancePooled
         );
-
-//        if (migrating != address(0)) {
-//            ICurveValueSet targetCurve = ICurveValueSet(updater.getTargetCurve(id));
-//            uint256 targetCollateralReturned = targetCurve.calculateBurnReturn(
-//                _meTokensBurned,
-//                // collateralDepositedAfterFees, // TODO: do we need to calculate after fees?
-//                id,
-//                meToken.totalSupply(),
-//                balancePooled
-//            );
-//            collateralReturned = WeightedAverage.calculate(
-//                collateralReturned,
-//                targetCollateralReturned,
-//                startTime,
-//                block.timestamp,
-//                endTime
-//            );
-//        }
 
         uint256 feeRate;
         uint256 collateralMultiplier;
@@ -210,22 +108,9 @@ contract Foundry is IFoundry, Ownable, Initializable {
             collateralMultiplier = PRECISION + PRECISION * details.balanceLocked / IERC20(_meToken).totalSupply();
         } else {
             feeRate = fees.burnBuyerFee();
-            // TODO
-//            if (shifting > 0) {
-//                uint256 targetCollateralMultiplier = PRECISION - shifting;
-//                collateralMultiplier = WeightedAverage.calculate(
-//                    collateralMultiplier,
-//                    targetCollateralMultiplier,
-//                    startTime,
-//                    block.timestamp,
-//                    endTime
-//                );
-//            }
         }
 
         uint256 collateralReturnedWeighted = collateralReturned * collateralMultiplier / PRECISION;
-//        uint256 fee = collateralReturnedWeighted * feeRate / PRECISION;   // TODO: undo inlining this elsewhere - BAD CODE
-
 //        uint256 collateralReturnedAfterFees = collateralReturnedWeighted - (collateralReturnedWeighted * feeRate / PRECISION);
 
         // Burn metoken from user
@@ -254,12 +139,14 @@ contract Foundry is IFoundry, Ownable, Initializable {
             );
         }
 
-        // Transfer fees
+        // Transfer fees - TODO
         if ((collateralReturnedWeighted * feeRate / PRECISION) > 0) {
-            IVault(hub.getVault(details.id)).addFee(collateralReturnedWeighted * feeRate / PRECISION);
+            uint256 fee = collateralReturnedWeighted * feeRate / PRECISION;
+            vault.addFee(fee);
         }
 
         // Send collateral from vault
+        // IERC20 collateralToken = IERC20(vault.getCollateralAsset());
         // collateralAsset.transferFrom(address(vault), _recipient, collateralReturnedAfterFees);
     }
 }
