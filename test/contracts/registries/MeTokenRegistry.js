494ad9b1b545a98a428e70e3120a1c8497fa8701
Ryan Robison <RyanLikesCrypto@gmail.com>
2021-08-26 22:48:40 -0700

    WIP: integration test for MeTokenRegistry

--- test/contracts/registries/MeTokenRegistry.js
+++ test/contracts/registries/MeTokenRegistry.js
@@ -4,15 +4,16 @@ const CurveRegistry = artifacts.require("CurveRegistry");
 const BancorZeroFormula = artifacts.require("BancorZeroFormula");
 const BancorZeroValueSet = artifacts.require("BancorZeroValueSet");
 const Hub = artifacts.require("Hub");
+const ERC20Mock = artifacts.require("ERC20Mock");
 
 
 describe("MeTokenRegistry.sol", () => {
 
     before(async () => {
-        
+
+        meTokenFactory = await MeTokenFactory.new(); // Should this be mocked?
         meTokenRegistry = await MeTokenRegistry.new(hub.address, meTokenFactory.address);
-        meTokenFactory = await MeTokenFactory.new();
-        
+
         // instantiate contracts to pass to initialize hub
         hub = await Hub.new();
         curveRegistry = await CurveRegistry.new();
@@ -27,6 +28,14 @@ describe("MeTokenRegistry.sol", () => {
     describe("register()", () => {
 
         it("User can create a meToken with no collateral", async () => {
+            // when(hub.getStatus(any)).thenReturn(expectedHubStatus)
+            // when(hub.getVault(any)).thenReturn(expectedHubVault)
+            erc20Mock = await ERC20Mock.new("mock", "MOCK", 0, 0);
+            // when(erc20Mock.balanceOf(any)).thenReturn(0)
+            // when(hub.getCurve(any)).thenReturn(expectedHubCurve)
+            // when(curve.calculateMintReturn(any, any, any, any)).thenReturn(0)
+
+            // when(erc20Mock.transferFrom(any)).thenReturn(0)  <- this is expected NOT to happen
             await meTokenRegistry.register
         });
 
