e989cbd36da8668bf645ad7f04475fc31c142c52
Carl Farterson <carlfarterson@gmail.com>
2021-08-27 08:00:38 +0200

    Create: init some mocks

---
+++ test/contracts/mocks/HubMock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract HubMock {
+    function getStatus(uint256 _hub) external pure returns (bool) {
+        return true;
+    }
+    function getVault(uint256 _hub) external pure returns (address) {
+        return address(0);
+    }
+    function getCurve(uint256 _hub, address curve) external pure returns (address) {return curve;}
+}
