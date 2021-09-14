bd6d3455b0136f59a47cd9d1d0951395f70e63bb
Carl Farterson <carlfarterson@gmail.com>
2021-09-13 22:00:52 -0600

    Add: calculateBurnReturn()

--- contracts/Foundry.sol
+++ contracts/Foundry.sol
@@ -56,7 +56,7 @@ contract Foundry is IFoundry, Ownable, Initializable {
             }
         }
 
-        uint meTokensMinted = calculateMintReturn(tokensDepositedAfterFees, _meToken, meTokenDetails, hubDetails);
+        uint meTokensMinted = calculateMintReturn(_meToken, tokensDepositedAfterFees,  meTokenDetails, hubDetails);
 
         // Send tokens to vault and update balance pooled
         address vaultToken = IVault(hubDetails.vault).getToken();
@@ -78,8 +78,8 @@ contract Foundry is IFoundry, Ownable, Initializable {
 
     // NOTE: for now this does not include fees
     function calculateMintReturn(
-        uint _tokensDeposited,
         address _meToken,
+        uint _tokensDeposited,
         MeTokenDetails memory _meTokenDetails,
         HubDetails memory _hubDetails
     ) public view returns (uint meTokensMinted) {
@@ -93,20 +93,19 @@ contract Foundry is IFoundry, Ownable, Initializable {
         );
 
         // Logic for if we're switching to a new curve type // updating curveDetails
-        if (
-            _hubDetails.updating &&
-            (_hubDetails.targetCurve != address(0)) || (_hubDetails.curveDetails))
-        {
+        if (_hubDetails.updating &&
+            (_hubDetails.targetCurve != address(0)) || (_hubDetails.curveDetails)
+        ) {
 
             uint targetMeTokensMinted;
-            if(_hubDetails.targetCurve != address(0)) {
+            if(_hubDetails.targetCurve != address(0)) { // Means we are updating to a new curve type
                 targetMeTokensMinted = ICurve(_hubDetails.targetCurve).calculateMintReturn(
                     _tokensDeposited,
                     _meTokenDetails.hubId,
                     IERC20(_meToken).totalSupply(),
                     _meTokenDetails.balancePooled
                 );
-            } else { // _hubDetails.curveDetails == true
+            } else { // Must mean we're updating curveDetails
                 targetMeTokensMinted = ICurve(_hubDetails.curve).calculateTargetMintReturn(
                     _tokensDeposited,
                     _meTokenDetails.hubId,
@@ -123,39 +122,50 @@ contract Foundry is IFoundry, Ownable, Initializable {
         }
     }
 
+    function calculateBurnReturn(
+        address _meToken,
+        uint _meTokensBurned,
+        MeTokenDetails memory _meTokenDetails,
+        HubDetails memory _hubDetails
+    ) public view returns (uint tokensReturned) {
 
-    // function mint(address _meToken, uint256 _tokensDeposited, address _recipient) external override {
+        // Calculate return assuming update is not happening
+        tokensReturned = ICurve(_hubDetails.curve).calculateBurnReturn(
+            _meTokensBurned,
+            _meTokenDetails.hubId,
+            IERC20(_meToken).totalSupply(),
+            _meTokenDetails.balancePooled
+        );
+  
+        // Logic for if we're switching to a new curve type // updating curveDetails
+        if (_hubDetails.updating &&
+            (_hubDetails.targetCurve != address(0)) || (_hubDetails.curveDetails)
+        ) {
 
-    //     MeTokenDetails memory meTokenDetails = meTokenRegistry.getDetails(_meToken);
-    //     HubDetails memory hubDetails = hub.getDetails(meTokenDetails.hubId);
-    //     require(hubDetails.active, "Hub inactive");
-
-    //     uint256 fee = _tokensDeposited * fees.mintFee() / PRECISION;
-    //     uint256 tokensDepositedAfterFees = _tokensDeposited - fee;
-
-    //     uint256 meTokensMinted = ICurve(hubDetails.curve).calculateMintReturn(
-    //         tokensDepositedAfterFees,
-    //         meTokenDetails.hubId,
-    //         IERC20(_meToken).totalSupply(),
-    //         meTokenDetails.balancePooled
-    //     );
-
-    //     // Send tokens to vault and update balance pooled
-    //     address vaultToken = IVault(hubDetails.vault).getToken();
-    //     IERC20(vaultToken).transferFrom(msg.sender, address(this), _tokensDeposited);
-
-    //     meTokenRegistry.incrementBalancePooled(
-    //         true,
-    //         _meToken,
-    //         tokensDepositedAfterFees
-    //     );
-
-    //     // Transfer fees
-    //     if (fee > 0) {IVault(hubDetails.vault).addFee(fee);}
-
-    //     // Mint meToken to user
-    //     IERC20(_meToken).mint(_recipient, meTokensMinted);
-    // }
+            uint targetTokensReturned;
+            if(_hubDetails.targetCurve != address(0)) { // Means we are updating to a new curve type
+                targetTokensReturned = ICurve(_hubDetails.targetCurve).calculateMintReturn(
+                    _meTokensBurned,
+                    _meTokenDetails.hubId,
+                    IERC20(_meToken).totalSupply(),
+                    _meTokenDetails.balancePooled
+                );
+            } else { // Must mean we're updating curveDetails
+                targetTokensReturned = ICurve(_hubDetails.curve).calculateTargetMintReturn(
+                    _meTokensBurned,
+                    _meTokenDetails.hubId,
+                    IERC20(_meToken).totalSupply(),
+                    _meTokenDetails.balancePooled
+                );                    
+            }
+            tokensReturned = WeightedAverage.calculate(
+                tokensReturned,
+                targetTokensReturned,
+                _hubDetails.startTime,
+                _hubDetails.endTime
+            );
+        }
+    }
 
 
     /// @inheritdoc IFoundry
@@ -166,12 +176,7 @@ contract Foundry is IFoundry, Ownable, Initializable {
         require(hubDetails.active, "Hub inactive");
 
         // Calculate how many tokens tokens are returned
-        uint256 tokensReturned = ICurve(hubDetails.curve).calculateBurnReturn(
-            _meTokensBurned,
-            meTokenDetails.hubId,
-            IERC20(_meToken).totalSupply(),
-            meTokenDetails.balancePooled
-        );
+        uint256 tokensReturned = calculateBurnReturn(_meToken, _meTokensBurned, meTokenDetails, hubDetails);
 
         uint256 feeRate;
         uint256 tokensMultiplier;
