b51495fba2b6c125635988303b1a33d47d214a4d
Carl Farterson <carlfarterson@gmail.com>
2021-10-12 15:43:34 -0700

    feat(migration): fees and Vault inheritance

--- contracts/vaults/SingleAssetVault.sol
+++ contracts/vaults/SingleAssetVault.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
+import "./Vault.sol";
 import "../libs/Details.sol";
 import "../interfaces/IVaultRegistry.sol";
 import "../interfaces/IMeTokenRegistry.sol";
@@ -15,13 +16,10 @@ import "hardhat/console.sol";
 /// @title Vault
 /// @author Carl Farterson (@carlfarterson)
 /// @notice Implementation contract for SingleAssetFactory.sol
-contract SingleAssetVault is IVault, Ownable, Initializable {
+contract SingleAssetVault is Vault, Initializable {
     uint256 public constant PRECISION = 10**18;
 
     bool public migrated;
-    address public token;
-    uint256 public accruedFees;
-    bytes public encodedAdditionalArgs;
 
     // TODO: figure out where to set these
     IMeTokenRegistry public meTokenRegistry = IMeTokenRegistry(address(0));
@@ -38,13 +36,6 @@ contract SingleAssetVault is IVault, Ownable, Initializable {
 
         // Approve Foundry to spend all collateral in vault
         IERC20(token).approve(_foundry, 2**256 - 1);
-    }
-
-    /// @inheritdoc IVault
-    function addFee(uint256 _amount) external override {
-        // TODO: access control
-        accruedFees += _amount;
-        emit AddFee(_amount);
     }
 
     // NOTE: this is only callable by hub
@@ -77,35 +68,5 @@ contract SingleAssetVault is IVault, Ownable, Initializable {
         address tokenToTransfer = IVault(hub_.vault).getToken();
 
         IERC20(tokenToTransfer).transfer(_migration, amtToTransfer);
-    }
-
-    /// @inheritdoc IVault
-    function withdraw(
-        bool _max,
-        uint256 _amount,
-        address _to
-    ) external override onlyOwner {
-        if (_max) {
-            _amount = accruedFees;
-        } else {
-            require(
-                _amount <= accruedFees,
-                "_amount cannot exceed accruedFees"
-            );
-        }
-
-        accruedFees -= _amount;
-
-        IERC20(token).transfer(_to, _amount);
-        emit Withdraw(_amount, _to);
-    }
-
-    function getAccruedFees() external view override returns (uint256) {
-        return accruedFees;
-    }
-
-    /// @inheritdoc IVault
-    function getToken() external view override returns (address) {
-        return token;
     }
 }
