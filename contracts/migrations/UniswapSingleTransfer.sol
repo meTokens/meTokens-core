a601ef171d58b616e69c0662421384eb5570f8eb
Carl Farterson <carlfarterson@gmail.com>
2021-10-26 15:20:55 -0700

    feat: most of initMigration() and startMigration()

--- contracts/migrations/UniswapSingleTransfer.sol
+++ contracts/migrations/UniswapSingleTransfer.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 import "../interfaces/IHub.sol";
 import "../interfaces/IMeTokenRegistry.sol";
+import "../interfaces/IMigrationRegistry.sol";
 import "../libs/Details.sol";
 import "../vaults/Vault.sol";
 
@@ -29,16 +30,19 @@ contract UniswapSingleTransfer is Initializable, Ownable, Vault {
     uint24 public immutable fee = 3000; // NOTE: 0.3% - the default uniswap fee
     IHub public hub;
     IMeTokenRegistry public meTokenRegistry;
+    IMigrationRegistry public migrationRegistry;
     uint256 public slippage;
 
     constructor(
         address _dao,
         address _foundry,
         IHub _hub,
-        IMeTokenRegistry _meTokenRegistry
+        IMeTokenRegistry _meTokenRegistry,
+        IMigrationRegistry _migrationRegistry
     ) Vault(_dao, _foundry) {
         hub = _hub;
         meTokenRegistry = _meTokenRegistry;
+        migrationRegistry = _migrationRegistry;
     }
 
     function register(uint256 _hubId, bytes memory _encodedArgs)
@@ -59,16 +63,80 @@ contract UniswapSingleTransfer is Initializable, Ownable, Vault {
         return IVault(hub_.vault).getAsset(_hubId);
     }
 
-    function initMigration(address _meToken, bytes memory _encodedArgs)
-        external
-    {}
+    // Kicks off meToken warmup period
+    function initMigration(
+        address _meToken,
+        address _migration,
+        uint256 _targetHubId
+    ) external {
+        Details.MeToken memory meToken_ = meTokenRegistry.getDetails(_meToken);
+        Details.Hub memory hub_ = hub.getDetails(meToken_.hubId);
+        Details.Hub memory targetHub_ = hub.getDetails(meToken_.targetHubId);
+
+        require(targetHub_.active, "Inactive _targetHubId");
+
+        // Make sure initial hub, migration, and target hub are a valid path
+        require(
+            migrationRegistry.isApproved(
+                hub_.vault,
+                _migration,
+                targetHub_.vault
+            ),
+            "invalid migration path"
+        );
+
+        // Get asset of initialHub and targetHub, If they're the same w/ no migration address, we're good
+        // TODO: is this needed
+        address initialAsset = IVault(hub_.vault).getAsset(meToken_.hubId);
+        address targetAsset = IVault(targetHub_.vault).getAsset(
+            meToken_.targetHubId
+        );
+
+        // NOTE: Target hub already knows the asset you're migrating to
+        // Set meToken startTime, endTime, endCooldown, targetHubId, migration
+        // meTokenRegistry.
+    }
+
+    // Warmup period has ended, send asset to migrationVault
+    function startMigration(address _meToken) external {
+        Details.MeToken memory meToken_ = meTokenRegistry.getDetails(_meToken);
+        Details.Hub memory hub_ = hub.getDetails(meToken_.hubId);
+        Details.UniswapSingleTransfer storage ust_ = usts[_meToken];
+
+        require(
+            block.timestamp > meToken_.startTime,
+            "Too soon to start migration"
+        );
+        require(ust_.amountIn == 0, "Already started");
+
+        // get asset used as collateral
+        address initialAsset = IVault(hub_.vault).getAsset(meToken_.hubId);
+        uint256 balance = meToken_.balancePooled + meToken_.balanceLocked;
+
+        // Only transfer to migrationVault if there is one
+        if (meToken_.migration != address(0)) {
+            IERC20(initialAsset).transferFrom(
+                hub_.vault,
+                address(this),
+                balance
+            );
+        }
+
+        ust_.amountIn = balance;
+    }
 
     function poke(address _meToken) external {
-        swap(_meToken);
+        Details.MeToken memory meToken_ = meTokenRegistry.getDetails(_meToken);
+        Details.UniswapSingleTransfer memory ust_ = usts[_meToken];
+
+        if (block.timestamp > meToken_.startTime && ust_.amountOut == 0) {
+            swap(_meToken);
+        }
     }
 
     function finishMigration(address _meToken) external {
         Details.UniswapSingleTransfer storage ust_ = usts[_meToken];
+        // TODO: require migration hasn't finished, block.timestamp > meToken_.startTime
 
         uint256 amountOut;
         if (ust_.amountOut > 0) {
@@ -90,27 +158,37 @@ contract UniswapSingleTransfer is Initializable, Ownable, Vault {
     }
 
     function swap(address _meToken) public returns (uint256) {
+        // TODO: only swap if initialAsset !=
         Details.UniswapSingleTransfer storage ust_ = usts[_meToken];
         Details.MeToken memory meToken_ = meTokenRegistry.getDetails(_meToken);
+        Details.Hub memory hub_ = hub.getDetails(meToken_.hubId);
+        Details.Hub memory targetHub_ = hub.getDetails(meToken_.targetHubId);
 
         require(ust_.amountIn > 0, "No amountIn");
         require(ust_.amountOut == 0, "Already swapped");
 
-        address initialToken = getVaultAsset(meToken_.hubId);
-        address targetToken = getVaultAsset(meToken_.targetHubId);
+        address initialAsset = IVault(hub_.vault).getAsset(meToken_.hubId);
+        address targetAsset = IVault(targetHub_.vault).getAsset(
+            meToken_.targetHubId
+        );
         require(
-            targetToken != address(0),
+            targetAsset != address(0),
             "MeToken does not have a target hub"
         );
+
+        // Only swap if there's a change in asset
+        if (initialAsset == targetAsset) {
+            return 0;
+        }
 
         // amountIn = IERC20(token).balanceOf(address(this));
         // https://docs.uniswap.org/protocol/guides/swaps/single-swaps
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
-                tokenIn: initialToken,
-                tokenOut: targetToken,
+                tokenIn: initialAsset,
+                tokenOut: targetAsset,
                 fee: fee,
-                recipient: address(this), // TODO: target vault
+                recipient: address(this),
                 deadline: block.timestamp,
                 amountIn: ust_.amountIn,
                 amountOutMinimum: 0,
@@ -139,7 +217,7 @@ contract UniswapSingleTransfer is Initializable, Ownable, Vault {
             PRECISION;
     }
 
-    function getUniswapSingleTransferDetails(address _meToken)
+    function getDetails(address _meToken)
         external
         view
         returns (Details.UniswapSingleTransfer memory ust_)
