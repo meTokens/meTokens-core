025d1a1c1697d61f14f6d71d6936241a040381cd
Carl Farterson <carlfarterson@gmail.com>
2021-11-09 13:53:13 -0800

    test(refundRatio): add logic for during/after cooldown

--- test/integration/Hub/RefundRatioTest.ts
+++ test/integration/Hub/RefundRatioTest.ts
@@ -1,21 +1,13 @@
 import { ethers, getNamedAccounts } from "hardhat";
-import { CurveRegistry } from "../../artifacts/types/CurveRegistry";
-import { Foundry } from "../../artifacts/types/Foundry";
-import { Hub } from "../../artifacts/types/Hub";
-import { WeightedAverage } from "../../artifacts/types/WeightedAverage";
-import { VaultRegistry } from "../../artifacts/types/VaultRegistry";
-import { deploy, getContractAt } from "../utils/helpers";
+import { Foundry } from "../../../artifacts/types/Foundry";
+import { Hub } from "../../../artifacts/types/Hub";
+import { deploy, getContractAt } from "../../utils/helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Signer, BigNumber } from "ethers";
-import { BancorZeroCurve } from "../../artifacts/types/BancorZeroCurve";
-import { ERC20 } from "../../artifacts/types/ERC20";
-import { MeTokenFactory } from "../../artifacts/types/MeTokenFactory";
-import { MeTokenRegistry } from "../../artifacts/types/MeTokenRegistry";
-import { MigrationRegistry } from "../../artifacts/types/MigrationRegistry";
-import { SingleAssetVault } from "../../artifacts/types/SingleAssetVault";
-import { impersonate, mineBlock, passOneHour } from "../utils/hardhatNode";
-import { Fees } from "../../artifacts/types/Fees";
-import { MeToken } from "../../artifacts/types/MeToken";
+import { ERC20 } from "../../../artifacts/types/ERC20";
+import { MeTokenRegistry } from "../../../artifacts/types/MeTokenRegistry";
+import { impersonate, mineBlock, passOneHour } from "../../utils/hardhatNode";
+import { MeToken } from "../../../artifacts/types/MeToken";
 import { expect } from "chai";
 
 describe("Hub - update RefundRatio", () => {
@@ -23,21 +15,14 @@ describe("Hub - update RefundRatio", () => {
   let dai: ERC20;
   let daiHolder: Signer;
   let DAIWhale: string;
-  let weightedAverage: WeightedAverage;
   let meTokenRegistry: MeTokenRegistry;
-  let meTokenFactory: MeTokenFactory;
-  let bancorZeroCurve: BancorZeroCurve;
-  let curveRegistry: CurveRegistry;
-  let vaultRegistry: VaultRegistry;
-  let migrationRegistry: MigrationRegistry;
-  let singleAssetVault: SingleAssetVault;
-  let fees: Fees;
   let foundry: Foundry;
   let meToken: MeToken;
   let hub: Hub;
   let account0: SignerWithAddress;
   let account1: SignerWithAddress;
   let account2: SignerWithAddress;
+
   const hubId = 1;
   const name = "Carl0 meToken";
   const symbol = "CARL";
@@ -46,6 +31,7 @@ describe("Hub - update RefundRatio", () => {
   const amount = ethers.utils.parseEther("100");
   const initRefundRatio = 500000;
   const targetRefundRatio = 750000;
+
   // for 1 DAI we get 1000 metokens
   const baseY = ethers.utils.parseEther("1").mul(1000).toString();
   // weight at 50% linear curve
@@ -64,84 +50,16 @@ describe("Hub - update RefundRatio", () => {
     dai
       .connect(daiHolder)
       .transfer(account2.address, ethers.utils.parseEther("1000"));
-    weightedAverage = await deploy<WeightedAverage>("WeightedAverage");
-    bancorZeroCurve = await deploy<BancorZeroCurve>("BancorZeroCurve");
-    curveRegistry = await deploy<CurveRegistry>("CurveRegistry");
-    vaultRegistry = await deploy<VaultRegistry>("VaultRegistry");
-    migrationRegistry = await deploy<MigrationRegistry>("MigrationRegistry");
-
-    foundry = await deploy<Foundry>("Foundry", {
-      WeightedAverage: weightedAverage.address,
-    });
-
-    hub = await deploy<Hub>("Hub");
-    meTokenFactory = await deploy<MeTokenFactory>("MeTokenFactory");
-    meTokenRegistry = await deploy<MeTokenRegistry>(
-      "MeTokenRegistry",
-      undefined,
-      hub.address,
-      meTokenFactory.address,
-      migrationRegistry.address
-    );
-    singleAssetVault = await deploy<SingleAssetVault>(
-      "SingleAssetVault",
-      undefined, //no libs
-      account1.address, // DAO
-      foundry.address, // foundry
-      hub.address, // hub
-      meTokenRegistry.address, //IMeTokenRegistry
-      migrationRegistry.address //IMigrationRegistry
-    );
-
-    await curveRegistry.approve(bancorZeroCurve.address);
-    await vaultRegistry.approve(singleAssetVault.address);
-
-    fees = await deploy<Fees>("Fees");
-
-    await fees.initialize(0, 0, 0, 0, 0, 0);
-    await hub.initialize(vaultRegistry.address, curveRegistry.address);
-
-    const encodedCurveDetails = ethers.utils.defaultAbiCoder.encode(
-      ["uint256", "uint32"],
-      [baseY, reserveWeight]
-    );
-    const encodedVaultArgs = ethers.utils.defaultAbiCoder.encode(
-      ["address"],
-      [DAI]
-    );
-
-    // refund ratio of 50000 = 0.00000000000005 ETH
-    // max ratio is 1 ETH = 1000000000000000000
-    // refund ratio is therefor 0,000000000005 %
-    await hub.register(
-      DAI,
-      singleAssetVault.address,
-      bancorZeroCurve.address,
-      initRefundRatio, //refund ratio
-      encodedCurveDetails,
-      encodedVaultArgs
-    );
-    await hub.setWarmup(60 * 24);
-    await hub.setDuration(60 * 24);
-    await hub.setCooldown(60 * 24);
-
-    await foundry.initialize(
-      hub.address,
-      fees.address,
-      meTokenRegistry.address
-    );
 
     // register a metoken
     await dai.approve(foundry.address, amount);
     const tx = await meTokenRegistry
       .connect(account0)
       .subscribe(name, symbol, hubId, 0);
-    const meTokenAddr = await meTokenRegistry.getOwnerMeToken(account0.address);
 
     // assert token infos
+    const meTokenAddr = await meTokenRegistry.getOwnerMeToken(account0.address);
     meToken = await getContractAt<MeToken>("MeToken", meTokenAddr);
-    const FOUNDRY = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("FOUNDRY"));
-    await meTokenRegistry.grantRole(FOUNDRY, foundry.address);
 
     await hub.initUpdate(
       hubId,
@@ -151,13 +69,41 @@ describe("Hub - update RefundRatio", () => {
     );
   });
 
-  it("During warmup, tokens received based on initialRefundRatio", async () => {
-    // fastFwd an hr
-    let hubDetails = await hub.getDetails(hubId);
-    const balDaiBefore = await dai.balanceOf(account2.address);
+  describe("During warmup", () => {
+    it("initUpdate() cannot be called", async () => {
+      // TODO: fast fwd a little bit
+    });
+
+    it("Assets received based on initialRefundRatio", async () => {});
   });
 
-  it("During duration, tokens received based on weighted avg", async () => {});
+  describe("During duration", () => {
+    it("initUpdate() cannot be called", async () => {
+      // TODO: fast to active duration
+    });
 
-  it("During cooldown, burn() uses targetRefundRatio", async () => {});
+    it("Assets received for owner based on weighted average", async () => {});
+
+    it("Assets received for buyer based on weighted average", async () => {});
+  });
+
+  describe("During cooldown", () => {
+    it("initUpdate() cannot be called", async () => {
+      // TODO: fast fwd to active cooldown
+    });
+
+    it("Before refundRatio set, burn() should use the targetRefundRatio", async () => {});
+
+    it("Should update refundRatio to targetRefundRatio", async () => {});
+  });
+
+  describe("After cooldown", () => {
+    it("initUpdate() can be called again", async () => {
+      // TODO: fast fwd to after cooldown
+    });
+
+    it("If no burns during cooldown, new targetRefundRatio cannot equal hub.targetRefundRatio", async () => {});
+
+    it("If no burns during cooldown, set refundRatio to old targetRefundRatio", async () => {});
+  });
 });
