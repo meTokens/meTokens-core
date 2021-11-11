0e4cd9b93aec9d203bb2504ab9efa22311c49c6e
Carl Farterson <carlfarterson@gmail.com>
2021-11-11 13:58:20 -0800

    feat(test): write out shell hub tests

--- test/contracts/Hub.ts
+++ test/contracts/Hub.ts
@@ -1,64 +1,60 @@
 import { ethers } from "hardhat";
 import { Hub } from "../../artifacts/types/Hub";
-import { Foundry } from "../../artifacts/types/Foundry";
-import { CurveRegistry } from "../../artifacts/types/CurveRegistry";
-import { VaultRegistry } from "../../artifacts/types/VaultRegistry";
-import { WeightedAverage } from "../../artifacts/types/WeightedAverage";
-
-/*
-const paginationFactory = await ethers.getContractFactory("Pagination", {});
-const paginationLib = await paginationFactory.deploy();
-
-const policyFactory = await ethers.getContractFactory("PolicyLib", {
-  libraries: {
-    Pagination: paginationLib.address,
-  },
-});
-*/
 
 describe("Hub.sol", () => {
-  let hub: Hub;
-  let foundry: Foundry;
-  let curveRegistry: CurveRegistry;
-  let vaultRegistry: VaultRegistry;
+  before(async () => {});
 
-  before(async () => {
-    const hubFactory = await ethers.getContractFactory("Hub");
-    hub = (await hubFactory.deploy()) as Hub;
-    await hub.deployed();
+  describe("register()", () => {
+    // Do something
+  });
 
-    const weightedAverageLibFactory = await ethers.getContractFactory(
-      "WeightedAverage"
-    );
-    const weightedAverageLib = await weightedAverageLibFactory.deploy();
-    const foundryFactory = await ethers.getContractFactory("Foundry", {
-      libraries: { WeightedAverage: weightedAverageLib.address },
+  describe("initUpdate()", () => {
+    it("Cannot be called if all values are the same", async () => {});
+    it("Can be called a first time", async () => {
+      // TODO: call initUpdate()
     });
-    foundry = (await foundryFactory.deploy()) as Foundry;
-    await foundry.deployed();
 
-    const curveRegistryFactory = await ethers.getContractFactory(
-      "CurveRegistry"
-    );
-    curveRegistry = (await curveRegistryFactory.deploy()) as CurveRegistry;
-    await curveRegistry.deployed();
+    it("Cannot be called during warmup, duration, and cooldown", async () => {
+      // TODO: fast fwd to warmup, duration, cooldown and try expect
+      // calling initUpdate() to revert
+    });
 
-    const vaultRegistryFactory = await ethers.getContractFactory(
-      "VaultRegistry"
-    );
-    vaultRegistry = (await vaultRegistryFactory.deploy()) as VaultRegistry;
-    await vaultRegistry.deployed();
+    it("Sets target values to values if finishUpdate() never after from first update", async () => {});
+
+    it("Can be called a second time after cooldown", async () => {
+      // TODO: fast fwd to after cooldown and call initUpdate()
+    });
   });
 
-  it("initialize()", async () => {
-    // Do something
-  });
+  describe("finishUpdate()", () => {
+    it("Doesn't trigger during warmup or duration", async () => {
+      // TODO
+    });
 
-  it("register()", async () => {
-    // Do something
-  });
+    it("Trigger once when mint() called during cooldown", async () => {
+      // TODO
+    });
 
-  it("deactivate()", async () => {
-    // Do something
+    it("Trigger once when burn() called during cooldown", async () => {
+      // TODO
+    });
+
+    it("Trigger once when mint() called if no mint() / burn() called during cooldown", async () => {
+      // TODO
+    });
+
+    it("Trigger once when burn() called if no mint() / burn() called during cooldown", async () => {
+      // TODO
+    });
+
+    it("Correctly set HubDetails when called during cooldown", async () => {
+      // TODO
+    });
+    it("Correctly set HubDetails when called after cooldown", async () => {
+      // TODO
+    });
+    it("Correctly set HubDetails when called during second initUpdate()", async () => {
+      // TODO
+    });
   });
 });
