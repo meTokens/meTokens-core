bd7431ff21f64f849e2a477b3bca5c811e8be729
Benjamin <benjamin.mateo@protonmail.com>
2021-11-24 17:31:44 +0100

    fix(test): curve

--- contracts/vaults/Vault.sol
+++ contracts/vaults/Vault.sol
@@ -40,7 +40,10 @@ abstract contract Vault is Ownable, IVault {
     }
 
     function approveAsset(address _asset, uint256 _amount) external override {
-        require(msg.sender == foundry, "!foundry");
+        require(
+            msg.sender == foundry || msg.sender == address(meTokenRegistry),
+            "!foundry||!meTokenRegistry"
+        );
         // increase the allowance to be able to burn tokens and retrieve the collateral
         IERC20(_asset).safeIncreaseAllowance(foundry, _amount);
     }
