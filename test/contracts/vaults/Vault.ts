47b23d9443c2692dbe6bdcc47492272619f87bee
Parv <parv3213@gmail.com>
2021-12-14 21:18:24 +0530

    test(Vault): add incremental tests

--- test/contracts/vaults/Vault.ts
+++ test/contracts/vaults/Vault.ts
@@ -7,17 +7,24 @@ import { MeTokenFactory } from "../../../artifacts/types/MeTokenFactory";
 import { MeTokenRegistry } from "../../../artifacts/types/MeTokenRegistry";
 import { MigrationRegistry } from "../../../artifacts/types/MigrationRegistry";
 import { WeightedAverage } from "../../../artifacts/types/WeightedAverage";
-import { deploy } from "../../utils/helpers";
+import { deploy, getContractAt } from "../../utils/helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { hubSetup } from "../../utils/hubSetup";
+import { CurveRegistry } from "../../../artifacts/types/CurveRegistry";
+import { BancorABDK } from "../../../artifacts/types/BancorABDK";
+import { ERC20 } from "../../../artifacts/types/ERC20";
+import { BigNumber, Signer } from "ethers";
+import { MeToken } from "../../../artifacts/types/MeToken";
+import { validateAndParseAddress } from "@uniswap/sdk-core";
+import { Fees } from "../../../artifacts/types/Fees";
 
 describe("Vault.sol", () => {
-  const amount = 3;
+  let token: ERC20;
   let vault: SingleAssetVault;
   let DAI: string;
   let account0: SignerWithAddress;
   let account1: SignerWithAddress;
   let account2: SignerWithAddress;
-
   let dao: SignerWithAddress;
   let weightedAverage: WeightedAverage;
   let migrationRegistry: MigrationRegistry;
@@ -25,39 +32,65 @@ describe("Vault.sol", () => {
   let hub: Hub;
   let meTokenFactory: MeTokenFactory;
   let meTokenRegistry: MeTokenRegistry;
+  let curve: BancorABDK;
+  let curveRegistry: CurveRegistry;
+  let tokenHolder: Signer;
+  let meToken: MeToken;
+  let fees: Fees;
+  let accruedFee: BigNumber;
 
+  const amount = ethers.utils.parseEther("100");
   const precision = ethers.utils.parseUnits("1");
+  const initRefundRatio = 50000;
+  const MAX_WEIGHT = 1000000;
+  const reserveWeight = MAX_WEIGHT / 2;
+  const baseY = precision.div(1000);
+  const hubId = 1;
   before(async () => {
     ({ DAI } = await getNamedAccounts());
 
     [account0, account1, account2] = await ethers.getSigners();
-    dao = account1;
+    dao = account0;
     weightedAverage = await deploy<WeightedAverage>("WeightedAverage");
-    migrationRegistry = await deploy<MigrationRegistry>("MigrationRegistry");
 
-    foundry = await deploy<Foundry>("Foundry", {
-      WeightedAverage: weightedAverage.address,
-    });
-    hub = await deploy<Hub>("Hub");
-    meTokenFactory = await deploy<MeTokenFactory>("MeTokenFactory");
-    meTokenRegistry = await deploy<MeTokenRegistry>(
-      "MeTokenRegistry",
-      undefined,
-      foundry.address,
-      hub.address,
-      meTokenFactory.address,
-      migrationRegistry.address
+    const encodedVaultArgs = ethers.utils.defaultAbiCoder.encode(
+      ["address"],
+      [DAI]
     );
+    const encodedCurveDetails = ethers.utils.defaultAbiCoder.encode(
+      ["uint256", "uint32"],
+      [baseY, reserveWeight]
+    );
+    curve = await deploy<BancorABDK>("BancorABDK");
+    ({
+      token,
+      tokenHolder,
+      hub,
+      foundry,
+      account0,
+      account1,
+      account2,
+      meTokenRegistry,
+      curveRegistry,
+      migrationRegistry,
+      singleAssetVault: vault,
+      fee: fees,
+    } = await hubSetup(
+      encodedCurveDetails,
+      encodedVaultArgs,
+      initRefundRatio,
+      curve
+    ));
 
-    vault = await deploy<SingleAssetVault>(
-      "SingleAssetVault",
-      undefined,
-      dao.address, // DAO
-      foundry.address, // foundry
-      hub.address, // hub
-      meTokenRegistry.address, //IMeTokenRegistry
-      migrationRegistry.address //IMigrationRegistry
-    );
+    await token.connect(tokenHolder).transfer(account0.address, amount);
+    await token.connect(tokenHolder).transfer(account1.address, amount);
+    await token.connect(tokenHolder).transfer(account2.address, amount);
+
+    await meTokenRegistry.subscribe("METOKEN", "MT", hubId, 0);
+    const meTokenAddr = await meTokenRegistry.getOwnerMeToken(account0.address);
+
+    meToken = await getContractAt<MeToken>("MeToken", meTokenAddr);
+    await fees.setMintFee(1e8);
   });
 
   describe("Check initial state", () => {
@@ -73,7 +106,7 @@ describe("Vault.sol", () => {
       expect(await vault.migrationRegistry()).to.be.equal(
         migrationRegistry.address
       );
-      expect(await vault.accruedFees(dao.address)).to.be.equal(0);
+      expect(await vault.accruedFees(DAI)).to.be.equal(0);
     });
   });
 
@@ -90,27 +123,56 @@ describe("Vault.sol", () => {
       // FIXME not a valid test
     });
     it("Increments accruedFees revert if not foundry", async () => {
-      await expect(vault.addFee(DAI, amount)).to.be.revertedWith("!foundry");
+      await expect(
+        vault.connect(account1).addFee(DAI, amount)
+      ).to.be.revertedWith("!foundry");
     });
-  });
+    it("should be call addFee() from foundry", async () => {
+      await token.approve(foundry.address, amount);
+      const tx = await foundry.mint(meToken.address, amount, account1.address);
+      await tx.wait();
 
-  describe("withdrawFees()", () => {
-    xit("Reverts when not called by owner", async () => {
-      // TODO
-    });
+      accruedFee = (await fees.mintFee()).mul(amount).div(precision);
+      await expect(tx).to.emit(vault, "AddFee").withArgs(DAI, accruedFee);
 
-    xit("Transfer some accrued fees", async () => {
-      // TODO
-    });
-
-    xit("Transfer all remaining accrued fees", async () => {
-      // TODO
+      expect(await vault.accruedFees(DAI)).to.be.equal(accruedFee);
     });
   });
 
   describe("withdraw()", () => {
-    it("reverts when sender is not dao", async () => {
-      await expect(vault.withdraw(DAI, true, 0)).to.be.revertedWith("!DAO");
+    it("Reverts when not called by owner", async () => {
+      await expect(
+        vault.connect(account1).withdraw(DAI, true, 0)
+      ).to.be.revertedWith("!DAO");
+    });
+
+    it("should revert when try to withdraw more than accruedFees[_asset]", async () => {
+      await expect(
+        vault.withdraw(DAI, false, accruedFee.add(1))
+      ).to.be.revertedWith("");
+    });
+
+    it("Transfer some accrued fees", async () => {
+      const amountToWithdraw = accruedFee.div(2);
+      const daoBalanceBefore = await token.balanceOf(dao.address);
+
+      const tx = await vault.withdraw(DAI, false, amountToWithdraw);
+      await tx.wait();
+
+      await expect(tx)
+        .to.emit(vault, "Withdraw")
+        .withArgs(DAI, amountToWithdraw);
+
+      const daoBalanceAfter = await token.balanceOf(dao.address);
+      accruedFee = accruedFee.sub(amountToWithdraw);
+      expect(await vault.accruedFees(DAI)).to.be.equal(accruedFee);
+      expect(daoBalanceAfter.sub(daoBalanceBefore)).to.be.equal(
+        amountToWithdraw
+      );
+    });
+
+    xit("Transfer all remaining accrued fees", async () => {
+      // TODO
     });
   });
 });
