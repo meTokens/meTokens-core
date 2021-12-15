491789d6fc6799614f190c9a11db39cd8bd31015
Parv <parv3213@gmail.com>
2021-12-15 20:40:39 +0530

    fix(USTM): bug and optimisation in poke

--- contracts/migrations/UniswapSingleTransferMigration.sol
+++ contracts/migrations/UniswapSingleTransferMigration.sol
@@ -74,10 +74,14 @@ contract UniswapSingleTransferMigration is
 
     function poke(address _meToken) external override {
         // Make sure meToken is in a state of resubscription
-        UniswapSingleTransfer memory usts_ = _uniswapSingleTransfers[_meToken];
+        UniswapSingleTransfer storage usts_ = _uniswapSingleTransfers[_meToken];
         Details.MeToken memory meToken_ = meTokenRegistry.getDetails(_meToken);
         Details.Hub memory hub_ = hub.getDetails(meToken_.hubId);
-        if (usts_.soonest != 0 && block.timestamp > usts_.soonest) {
+        if (
+            usts_.soonest != 0 &&
+            block.timestamp > usts_.soonest &&
+            !usts_.started
+        ) {
             ISingleAssetVault(hub_.vault).startMigration(_meToken);
             usts_.started = true;
             _swap(_meToken);
