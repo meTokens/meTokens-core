42588cc125c41fdcf2dee19b2ba89fa0956d5e35
Parv <parv3213@gmail.com>
2021-12-28 21:11:54 +0530

    fix(Foundry): extra code
    - i suppose this got mistakenly added by me due to rebase

--- contracts/Foundry.sol
+++ contracts/Foundry.sol
@@ -70,7 +70,6 @@ contract Foundry is IFoundry, Ownable, Initializable {
             if (block.timestamp > meToken_.endTime) {
                 hub_ = hub.getDetails(meToken_.targetHubId);
                 meToken_ = meTokenRegistry.finishResubscribe(_meToken);
-                hub_ = hub.getDetails(meToken_.hubId);
             } else if (block.timestamp > meToken_.startTime) {
                 // Handle migration actions if needed
                 IMigration(meToken_.migration).poke(_meToken);
@@ -169,7 +168,6 @@ contract Foundry is IFoundry, Ownable, Initializable {
         ) {
             hub_ = hub.getDetails(meToken_.targetHubId);
             meToken_ = meTokenRegistry.finishResubscribe(_meToken);
-            hub_ = hub.getDetails(meToken_.hubId);
         }
         // Calculate how many tokens are returned
         uint256 rawAssetsReturned = calculateRawAssetsReturned(
