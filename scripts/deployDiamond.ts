569db8d269572ee7b1b80e0329b016c14ce813e7
Carl Farterson <carlfarterson@gmail.com>
2022-01-10 17:15:36 -0800

    fix: basic diamond deploy script in ts

--- scripts/deployDiamond.ts
+++ scripts/deployDiamond.ts
@@ -1,11 +1,19 @@
 import { deploy } from "../test/utils/helpers";
 import { network, run, ethers, getNamedAccounts } from "hardhat";
-import fs from "fs";
+import { DiamondCutFacet } from "../artifacts/types/DiamondCutFacet";
+import { Diamond } from "../artifacts/types/Diamond";
+import { DiamondInit } from "../artifacts/types/DiamondInit";
+import { DiamondLoupeFacet } from "../artifacts/types/DiamondLoupeFacet";
+import { OwnershipFacet } from "../artifacts/types/OwnershipFacet";
+import { getSelectors } from "./libraries/helpers";
+
+const FacetCutAction = { Add: 0, Replace: 1, Remove: 2 };
 const ETHERSCAN_CHAIN_IDS = [1, 3, 4, 5, 42];
 const SUPPORTED_NETWORK = [1, 4, 100, 31337];
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  const deployerAddr = await deployer.getAddress();
 
   const address = await deployer.getAddress();
   if (!deployer.provider) {
@@ -15,6 +23,61 @@ async function main() {
 
   if (SUPPORTED_NETWORK.indexOf(chainId) === -1)
     throw new Error("Un-supported network");
+
+  console.log("Deploying on network", network.name);
+  console.log("Deployer address:", deployerAddr);
+
+  const diamondCutFacet = await deploy<DiamondCutFacet>("DiamondCutFacet");
+  console.log("\nDiamondCutFacet deployed at:", diamondCutFacet.address);
+
+  const diamond = await deploy<Diamond>(
+    "Diamond",
+    undefined,
+    deployer.address,
+    diamondCutFacet.address
+  );
+  console.log("Diamond deployed at:", diamond.address);
+
+  const diamondInit = await deploy<DiamondInit>("DiamondInit");
+  console.log("DiamondInit deployed at:", diamondInit.address);
+
+  // deploy facets
+  console.log("\nDeploying Facets");
+  const FacetNames = ["DiamondLoupeFacet", "OwnershipFacet"];
+  const cut = [];
+  for (const FacetName of FacetNames) {
+    const Facet = await ethers.getContractFactory(FacetName);
+    const facet = await Facet.deploy();
+    await facet.deployed();
+    console.log(`${FacetName} deployed at: ${facet.address}`);
+    cut.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: getSelectors(facet),
+    });
+  }
+
+  // Upgrade diamond w/ facets
+  console.log("\nDiamond Cut:", cut);
+  const diamondCut = await ethers.getContractAt("IDiamondCut", diamond.address);
+  let tx;
+  let receipt;
+  let args: any = [
+    {
+      foundry: "0x00000000005dbcB0d0513FcDa746382Fe8a53468",
+      vaultRegistry: "0x00000000005dbcB0d0513FcDa746382Fe8a53468",
+      curveRegistry: "0x00000000005dbcB0d0513FcDa746382Fe8a53468",
+      migrationRegistry: "0x00000000005dbcB0d0513FcDa746382Fe8a53468",
+    },
+  ];
+  // call to init function
+  let functionCall = diamondInit.interface.encodeFunctionData("init", args);
+  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall);
+  console.log("Diamond cut tx: ", tx.hash);
+  receipt = await tx.wait();
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`);
+  }
 }
 
 main()
