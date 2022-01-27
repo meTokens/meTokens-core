a177c1df9237deaa12d8449078fded5cbeef21ee
Parv <parv3213@gmail.com>
2022-01-27 20:14:46 +0530

    feat: accept donations
    - need confirmation: donation fee as zero

--- contracts/interfaces/IFoundry.sol
+++ contracts/interfaces/IFoundry.sol
@@ -36,6 +36,18 @@ interface IFoundry {
         uint256 _assetsReturned
     );
 
+    /// @notice Event of donating to meToken owner
+    /// @param _meToken         address of meToken burned
+    /// @param _asset           address of asset returned
+    /// @param _donor           address donating the asset
+    /// @param _assetsDeposited amount of assets to c
+    event Donate(
+        address _meToken,
+        address _asset,
+        address _donor,
+        uint256 _assetsDeposited
+    );
+
     /// @notice Mint a meToken by depositing the underlying asset
     /// @param _meToken         address of meToken to mint
     /// @param _assetsDeposited amount of assets to deposit
@@ -55,4 +67,9 @@ interface IFoundry {
         uint256 _meTokensBurned,
         address _recipient
     ) external;
+
+    /// @notice Donate a meToken's underlying asset to its owner
+    /// @param _meToken         address of meToken to burn
+    /// @param _assetsDeposited amount of asset to donate
+    function donate(address _meToken, uint256 _assetsDeposited) external;
 }
