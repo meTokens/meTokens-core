e5480565487ed009b7f0d3b520f07078adecfe90
Benjamin <benjamin.mateo@protonmail.com>
2022-03-03 01:15:55 +0100

    fix(lint): update linting

--- contracts/mocks/UpdatedFeesFacet.sol
+++ contracts/mocks/UpdatedFeesFacet.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.9;
 import {ModifiersUpdated} from "./LibAppStorageUpdated.sol";
 
 contract UpdatedFeesFacet is ModifiersUpdated {
+    function setInterestFee(uint256 rate) external onlyFeesController {
+        require(rate != s.interestFee && rate < s.PRECISION, "out of range");
+        s.interestFee = rate + 42;
+    }
+
     function setTotallyNewAddress(address totallyNewAddr)
         external
         onlyFeesController
@@ -17,11 +22,6 @@ contract UpdatedFeesFacet is ModifiersUpdated {
 
     function interestPlusYieldFee() external view returns (uint256) {
         return s.interestFee + s.yieldFee;
-    }
-
-    function setInterestFee(uint256 rate) external onlyFeesController {
-        require(rate != s.interestFee && rate < s.PRECISION, "out of range");
-        s.interestFee = rate + 42;
     }
 
     function add(uint256 x, uint256 y) external view returns (uint256) {
