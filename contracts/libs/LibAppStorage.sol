824b6f477650d9b86f20234c2c6382f76d5fd905
Carter Carlson <cartercarlson@protonmail.com>
2022-04-12 16:13:59 -0700

    fix: all state to appStorage

--- contracts/libs/LibAppStorage.sol
+++ contracts/libs/LibAppStorage.sol
@@ -1,13 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.9;
 
-<<<<<<< HEAD
 import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import {IVaultRegistry} from "../interfaces/IVaultRegistry.sol";
-=======
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IRegistry} from "../interfaces/IRegistry.sol";
->>>>>>> 0342e61 (fix: change `me` type to IERC20)
 import {IMigrationRegistry} from "../interfaces/IMigrationRegistry.sol";
 import {HubInfo} from "./LibHub.sol";
 import {MeTokenInfo} from "./LibMeToken.sol";
@@ -53,7 +48,7 @@ struct AppStorage {
     // Widely-used addresses/interfaces
     address diamond;
     address meTokenFactory;
-    IERC20 me;
+    IERC20Permit me;
     IVaultRegistry vaultRegistry;
     IMigrationRegistry migrationRegistry;
     // Controllers
