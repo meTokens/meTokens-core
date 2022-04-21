ca281138886842ac3436e50d39c46ff1d7fda682
Benjamin <benjamin.mateo@protonmail.com>
2022-04-21 12:01:26 +0200

    fix(rentrency): add to local storage

--- contracts/facets/LiquidityMiningFacet.sol
+++ contracts/facets/LiquidityMiningFacet.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.9;
 
 // Modified version of https://github.com/Synthetixio/synthetix/blob/develop/contracts/StakingRewards.sol
 
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
@@ -16,12 +15,33 @@ import "hardhat/console.sol";
 
 /// @author @cartercarlson, @bunsdev, @cbobrobison
 /// @title Rewards contract for meTokens liquidity mining
-contract LiquidityMiningFacet is
-    ILiquidityMiningFacet,
-    ReentrancyGuard,
-    Modifiers
-{
+contract LiquidityMiningFacet is ILiquidityMiningFacet, Modifiers {
     using SafeERC20 for IERC20;
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        LibLiquidityMining.LiquidityMiningStorage
+            storage ls = LibLiquidityMining.liquidityMiningStorage();
+        // On the first call to nonReentrant, _notEntered will be true
+        require(
+            ls.status != LibLiquidityMining._ENTERED,
+            "ReentrancyGuard: reentrant call"
+        );
+
+        // Any calls to nonReentrant after this point will fail
+        ls.status = LibLiquidityMining._ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        ls.status = LibLiquidityMining._NOT_ENTERED;
+    }
 
     function initSeason(
         uint256 initTime,
