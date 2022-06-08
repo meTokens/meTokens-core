013f8cd91b7cfc2088847098ffc91f07d5fe8a8b
Benjamin <benjamin.mateo@protonmail.com>
2022-06-09 00:08:50 +0200

    fix(LM): start fresh

--- contracts/libs/LibLiquidityMining.sol
+++ contracts/libs/LibLiquidityMining.sol
@@ -7,7 +7,6 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 struct LiquidityMiningStorage {
     // LiquidityMining-specific
     uint256 issuerCooldown; // # of seasons a meToken issuer has to wait before participating again
-    uint256 lmWarmup; // = 3 days; timeframe between initTime and startTime
     uint256 lmDuration; // = 1000000; // timeframe from a season starting to ending - about 11.5 days
     mapping(address => mapping(address => uint256)) stakedBalances; // key 1: meToken addr- key2: staker addr- value: amount staked
     mapping(address => PoolInfo) pools;
@@ -17,23 +16,17 @@ struct LiquidityMiningStorage {
 }
 struct PoolInfo {
     bytes32 seasonMerkleRoot; // used to identify if it is a new season
-    uint256 pendingIssuerRewards;
-    bool pendingIssuerRewardsAdded;
     uint256 lastUpdateTime;
     uint256 totalSupply; // supply staked
-    uint256 lastCirculatingSupply;
     uint256 rewardPerTokenStored;
     mapping(address => uint256) userRewardPerTokenPaid;
     mapping(address => uint256) rewards; // key: staker addr
 }
 
 struct SeasonInfo {
-    uint256 initTime;
     uint256 startTime;
     uint256 endTime;
     uint256 allocationPool; // allocation for each meToken in a season
-    uint256 allocationIssuers; // total allocation for issuers in a season
-    uint256 totalPctStaked;
     uint256 rewardRate;
     bytes32 merkleRoot;
 }
@@ -47,11 +40,8 @@ library LibLiquidityMining {
         view
         returns (
             bytes32 seasonMerkleRoot,
-            uint256 pendingIssuerRewards,
-            bool pendingIssuerRewardsAdded,
             uint256 lastUpdateTime,
             uint256 totalSupply,
-            uint256 lastCirculatingSupply,
             uint256 rewardPerTokenStored,
             uint256 userRewardPerTokenPaid,
             uint256 rewards
@@ -59,11 +49,8 @@ library LibLiquidityMining {
     {
         LiquidityMiningStorage storage ls = liquidityMiningStorage();
         seasonMerkleRoot = ls.pools[meToken].seasonMerkleRoot;
-        pendingIssuerRewards = ls.pools[meToken].pendingIssuerRewards;
-        pendingIssuerRewardsAdded = ls.pools[meToken].pendingIssuerRewardsAdded;
         lastUpdateTime = ls.pools[meToken].lastUpdateTime;
         totalSupply = ls.pools[meToken].totalSupply;
-        lastCirculatingSupply = ls.pools[meToken].lastCirculatingSupply;
         rewardPerTokenStored = ls.pools[meToken].rewardPerTokenStored;
         userRewardPerTokenPaid = ls.pools[meToken].userRewardPerTokenPaid[user];
         rewards = ls.pools[meToken].rewards[user];
